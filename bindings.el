111bd42ff00db8e2269ee70ca24389c3ff606bba
David Holm <david.holm@ericsson.com>
2011-07-08 10:42:21 +0200

    Use regular expression search by default

--- bindings.el
+++ bindings.el
@@ -32,3 +32,9 @@
 ;; Spell check the current word
 (global-set-key "\C-c\C-w" 'ispell-word)
 
+
+;; Use regex searches by default
+(global-set-key (kbd "C-s") 'isearch-forward-regexp)
+(global-set-key (kbd "\C-r") 'isearch-backward-regexp)
+(global-set-key (kbd "C-M-s") 'isearch-forward)
+(global-set-key (kbd "C-M-r") 'isearch-backward)
