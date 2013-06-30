c217855866d18c0084e71336eadacfd6ebc25688
David Holm <david.holm@ericsson.com>
2013-06-30 21:56:04 +0200

    Adds helper function for CEDET to compile standard GNU Make projects

--- .emacs.d/init/init-cedet.el
+++ .emacs.d/init/init-cedet.el
@@ -39,6 +39,14 @@
   (setq compilation-scroll-output t)
   (setq mode-compile-always-save-buffer-p t)
 
+  (defun dholm/gen-std-compile-string ()
+    "Generates compilation string for standard GNU Make project"
+    (let* ((current-dir (file-name-directory
+			 (or (buffer-file-name (current-buffer)) default-directory)))
+	   (prj (ede-current-project current-dir))
+	   (root-dir (ede-project-root-directory prj)))
+      (concat "cd " root-dir "; nice make -j")))
+
   (defun dholm/cedet-hook ()
     (local-set-key [(control return)] 'semantic-ia-complete-symbol)
     (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
