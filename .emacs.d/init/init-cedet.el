eb5ffe70db498f3d6fa97f5fea227fdd7bf3fd27
David Holm <david.holm@ericsson.com>
2013-07-03 01:22:15 +0200

    Enables looking up symbol references with C-cr when using semantic

--- .emacs.d/init/init-cedet.el
+++ .emacs.d/init/init-cedet.el
@@ -53,6 +53,7 @@
     (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
     (local-set-key "\C-c=" 'semantic-decoration-include-visit)
     (local-set-key "\C-cj" 'semantic-ia-fast-jump)
+    (local-set-key "\C-cr" 'semantic-symref)
     (local-set-key "\C-cq" 'semantic-ia-show-doc)
     (local-set-key "\C-cs" 'semantic-ia-show-summary)
     (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
