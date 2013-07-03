932ab95e436827c5bdf7935b9f2f8b21c1600f84
David Holm <dholmster@gmail.com>
2013-07-03 12:28:58 -0700

    Fixes issue in haskell mode

--- .emacs.d/modes/haskell.el
+++ .emacs.d/modes/haskell.el
@@ -1,5 +1,4 @@
 ;; Haskell mode
 
-(load "~/.emacs.d/el-get/haskell-mode/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
