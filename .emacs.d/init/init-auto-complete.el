cccdfa3b7cef6b4161675bf12cc63263e427e7d2
David Holm <dholmster@gmail.com>
2013-07-05 11:49:27 -0700

    Enables flyspell workaround when in auto-complete mode with flymake

--- .emacs.d/init/init-auto-complete.el
+++ .emacs.d/init/init-auto-complete.el
@@ -1,13 +1,21 @@
 (require 'auto-complete-config)
 
-(ac-config-default)
 (setq ac-auto-start nil
       ac-quick-help-delay 0.5
       ;; Store the completion history in the cache directory
-      ac-comphist-file "~/.emacs.cache/ac-comphist.dat")
+      ac-comphist-file "~/.emacs.cache/ac-comphist.dat"
+      ac-use-quick-help t)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/el-get/auto-complete/ac-dict")
-
 (add-hook 'auto-complete-mode-hook 'ac-common-setup)
+(ac-config-default)
+
+
+;; Workaround for flyspell mode
+(defun dholm/flymake-mode-hook ()
+  ;; auto-complete workaround
+  (ac-flyspell-workaround))
+(add-hook 'flymake-mode-hook 'dholm/flymake-mode-hook)
+
 
 ;; Enable auto-complete globally
 (global-auto-complete-mode t)
