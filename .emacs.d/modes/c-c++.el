2a42817a69e08f5a41cbcfa9cb60a74e315eb221
David Holm <dholmster@gmail.com>
2013-07-05 11:52:39 -0700

    Always enables dtrt-indent since we should always have it

--- .emacs.d/modes/c-c++.el
+++ .emacs.d/modes/c-c++.el
@@ -2,24 +2,21 @@
 (setq c-default-style "K&R")
 (setq c++-default-style "Stroustrup")
 
-;; Load the Google C/C++ style
-(require 'google-c-style)
 
-(add-hook 'c-mode-common-hook
-          (lambda ()
-            ;; Override the indentation level of case labels in the K&R- and
-            ;; Stroustrup styles so that they are indented one level beyond
-            ;; the switch.
-            (c-set-offset 'case-label '+)
-            ;; Run spell-checker on strings and comments
-            (flyspell-prog-mode)
-            ;; Separate camel-case into separate words
-            (subword-mode t)
-            ;; Show trailing whitespace
-            (setq show-trailing-whitespace t)
-            (add-hook 'before-save-hook
-                      ;; Delete trailing whitespace on save
-                      'delete-trailing-whitespace nil t)
-            (when (featurep 'dtrt-indent)
-              ;; Enable dtrt-indent to attempt to identify the indentation rules used
-              (dtrt-indent-mode t))))
+(defun dholm/c-mode-common-hook ()
+  ;; Override the indentation level of case labels in the K&R- and
+  ;; Stroustrup styles so that they are indented one level beyond
+  ;; the switch.
+  (c-set-offset 'case-label '+)
+  ;; Run spell-checker on strings and comments
+  (flyspell-prog-mode)
+  ;; Separate camel-case into separate words
+  (subword-mode t)
+  ;; Show trailing whitespace
+  (setq show-trailing-whitespace t)
+  (add-hook 'before-save-hook
+            ;; Delete trailing whitespace on save
+            'delete-trailing-whitespace nil t)
+  ;; Enable dtrt-indent to attempt to identify the indentation rules used
+  (dtrt-indent-mode t))
+(add-hook 'c-mode-common-hook 'dholm/c-mode-common-hook)
