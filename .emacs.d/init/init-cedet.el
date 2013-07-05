c73636b046ddb857049941c02817894e0c2adb54
David Holm <dholmster@gmail.com>
2013-07-05 13:29:11 -0700

    Cleans up CEDET configuration

--- .emacs.d/init/init-cedet.el
+++ .emacs.d/init/init-cedet.el
@@ -1,81 +1,76 @@
-(when (featurep 'cedet)
-  (load-file "~/.emacs.d/el-get/cedet/contrib/cedet-contrib-load.el")
-  (setq semanticdb-default-save-directory "~/.emacs.cache/semanticdb")
 
-  (require 'cedet-files)
-  (require 'cedet-graphviz)
-  (require 'cedet-global)
-  (require 'cedet-cscope)
+;; Set up paths to caches
+(setq semanticdb-default-save-directory "~/.emacs.cache/semanticdb"
+      ede-project-placeholder-cache-file "~/.emacs.cache/ede-projects.el"
+      srecode-map-save-file "~/.emacs.cache/srecode-map.el")
 
-  (semantic-mode t)
-  (semantic-load-enable-excessive-code-helpers)
-  (require 'semantic-tag-folding)
-  (require 'semantic/ia)
-  (require 'semantic/bovine/c)
-  (require 'semantic/bovine/gcc)
-  (require 'semantic/bovine/clang)
-  (require 'semantic/wisent/javascript)
-  (require 'semantic/wisent/python)
 
-  (require 'semantic/db)
-  (global-semanticdb-minor-mode t)
+;; Set up and enable semantic
+(require 'semantic/ia)
+(require 'semantic/db)
+(require 'semantic/bovine/c)
+(require 'semantic/bovine/gcc)
+(require 'semantic/bovine/clang)
+(require 'semantic/wisent/javascript)
+(require 'semantic/wisent/python)
 
+(semantic-mode t)
+(semantic-load-enable-excessive-code-helpers)
+(global-semanticdb-minor-mode t)
+
+
+;; Check if GNU Global is available
+(when (cedet-gnu-global-version-check t)
+  (semanticdb-enable-gnu-global-databases 'c-mode)
+  (semanticdb-enable-gnu-global-databases 'c++-mode))
+
+
+;; Check if ectag is available
+(when (cedet-ectag-version-check t)
+  (semantic-load-enable-primary-ctags-support))
+
+
+;; Enable SRecode templates globally
+(global-srecode-minor-mode t)
+
+
+;; Configure ede-mode project management
+(global-ede-mode t)
+(ede-enable-generic-projects)
+
+
+;; Hooks for setting up cedet when going into a supported mode
+(defun dholm/cedet-hook ()
+  (local-set-key [(control return)] 'semantic-ia-complete-symbol)
+  (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
+  (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
+  (local-set-key "\C-c=" 'semantic-decoration-include-visit)
+  (local-set-key "\C-cj" 'semantic-ia-fast-jump)
+  (local-set-key "\C-cr" 'semantic-symref)
+  (local-set-key "\C-cq" 'semantic-ia-show-doc)
+  (local-set-key "\C-cs" 'semantic-ia-show-summary)
+  (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
+  (local-set-key "\C-c+" 'semantic-tag-folding-show-block)
+  (local-set-key "\C-c-" 'semantic-tag-folding-fold-block)
+  (local-set-key "\C-c\C-c+" 'semantic-tag-folding-show-all)
+  (local-set-key "\C-c\C-c-" 'semantic-tag-folding-fold-all)
+  ;; Use semantic as a source for auto complete
+  (set (make-local-variable 'ac-sources)
+       (append ac-sources '(ac-source-semantic))))
+(add-hook 'c-mode-common-hook 'dholm/cedet-hook)
+(add-hook 'lisp-mode-hook 'dholm/cedet-hook)
+(add-hook 'emacs-lisp-mode-hook 'dholm/cedet-hook)
+(add-hook 'python-mode-hook 'dholm/cedet-hook)
+
+
+(defun dholm/c-mode-cedet-hook ()
+  (local-set-key "\C-ct" 'eassist-switch-h-cpp)
+  (local-set-key "\C-xt" 'eassist-switch-h-cpp)
+  (local-set-key "\C-ce" 'eassist-list-methods)
+  (local-set-key "\C-c\C-r" 'semantic-symref)
+  ;; Autocompletion
+  (auto-complete-mode t)
   (when (cedet-gnu-global-version-check t)
-    (semanticdb-enable-gnu-global-databases 'c-mode)
-    (semanticdb-enable-gnu-global-databases 'c++-mode))
-
-  (when (cedet-ectag-version-check t)
-    (semantic-load-enable-primary-ctags-support))
-
-  (require 'eassist)
-
-  (global-srecode-minor-mode t)
-
-  (global-ede-mode t)
-  (ede-enable-generic-projects)
-
-  (require 'compile)
-  (setq compilation-disable-input nil)
-  (setq compilation-scroll-output t)
-  (setq mode-compile-always-save-buffer-p t)
-
-  (defun dholm/gen-std-compile-string ()
-    "Generates compilation string for standard GNU Make project"
-    (let* ((current-dir (file-name-directory
-			 (or (buffer-file-name (current-buffer)) default-directory)))
-	   (prj (ede-current-project current-dir))
-	   (root-dir (ede-project-root-directory prj)))
-      (concat "cd " root-dir "; nice make -j")))
-
-  (defun dholm/cedet-hook ()
-    (local-set-key [(control return)] 'semantic-ia-complete-symbol)
-    (local-set-key "\C-c?" 'semantic-ia-complete-symbol-menu)
-    (local-set-key "\C-c>" 'semantic-complete-analyze-inline)
-    (local-set-key "\C-c=" 'semantic-decoration-include-visit)
-    (local-set-key "\C-cj" 'semantic-ia-fast-jump)
-    (local-set-key "\C-cr" 'semantic-symref)
-    (local-set-key "\C-cq" 'semantic-ia-show-doc)
-    (local-set-key "\C-cs" 'semantic-ia-show-summary)
-    (local-set-key "\C-cp" 'semantic-analyze-proto-impl-toggle)
-    (local-set-key "\C-c+" 'semantic-tag-folding-show-block)
-    (local-set-key "\C-c-" 'semantic-tag-folding-fold-block)
-    (local-set-key "\C-c\C-c+" 'semantic-tag-folding-show-all)
-    (local-set-key "\C-c\C-c-" 'semantic-tag-folding-fold-all)
-    ;; Use semantic as a source for auto complete
-    (set (make-local-variable 'ac-sources)
-         (append ac-sources '(ac-source-semantic))))
-  (add-hook 'c-mode-common-hook 'dholm/cedet-hook)
-  (add-hook 'lisp-mode-hook 'dholm/cedet-hook)
-  (add-hook 'emacs-lisp-mode-hook 'dholm/cedet-hook)
-
-  (defun dholm/c-mode-cedet-hook ()
-    (local-set-key "\C-ct" 'eassist-switch-h-cpp)
-    (local-set-key "\C-xt" 'eassist-switch-h-cpp)
-    (local-set-key "\C-ce" 'eassist-list-methods)
-    (local-set-key "\C-c\C-r" 'semantic-symref)
-    ;; Autocompletion
-    (auto-complete-mode t)
     (set (make-local-variable 'ac-sources)
          (append ac-sources '(ac-source-gtags)))))
-
 (add-hook 'c-mode-common-hook 'dholm/c-mode-cedet-hook)
