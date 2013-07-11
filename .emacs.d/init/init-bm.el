4f4d860d729a8ae6783b0e6148d26045ae2247ac
David Holm <dholmster@gmail.com>
2013-07-11 12:07:47 -0700

    Moves visual bookmarks' persistant data to the user's data directory

--- .emacs.d/init/init-bm.el
+++ .emacs.d/init/init-bm.el
@@ -1,17 +1,37 @@
 ;;; (Initialization) ;;;
 
-;; Make bookmarks persistent
-(setq-default bm-buffer-persistence t)
+
+(setq-default
+ ;; Put the repository in the data directory
+ bm-repository-file (path-join *user-data-directory* "bm-repository")
+ ;; Make bookmarks persistent by default
+ bm-buffer-persistence t
+ ;; Make sure the repository is loaded as early as possible
+ bm-restore-repository-on-load t)
+
 
 ;; The hooks are not autoloadable so bring in bm before registering them
 (require 'bm)
 
+
+;; Loading the repository from file when on start up.
 (add-hook' after-init-hook 'bm-repository-load)
+;; Restoring bookmarks when on file find.
 (add-hook 'find-file-hooks 'bm-buffer-restore)
+;; Saving bookmark data on killing a buffer
 (add-hook 'kill-buffer-hook 'bm-buffer-save)
+;; Saving the repository to file when on exit.
+;; kill-buffer-hook is not called when Emacs is killed, so we must save all
+;; bookmarks first.
 (add-hook 'kill-emacs-hook '(lambda ()
                               (bm-buffer-save-all)
                               (bm-repository-save)))
+;; Update bookmark repository when saving the file.
+(add-hook 'after-save-hook 'bm-buffer-save)
+;; Restore bookmarks when buffer is reverted.
+(add-hook 'after-revert-hook 'bm-buffer-restore)
+;; make sure bookmarks is saved before check-in (and revert-buffer)
+(add-hook 'vc-before-checkin-hook 'bm-buffer-save)
 
 
 ;;; (Bindings) ;;;
