a23bfccff65c847ce6ab33e3aa896f20bd01ddee
David Holm <dholmster@gmail.com>
2013-07-11 17:49:51 -0700

    package has to be initialized before bootstrapping el-get

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,6 +19,7 @@
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")))
+(package-initialize)
 
 
 ;; Configure and load el-get
