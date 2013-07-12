f906585ce22f002a7e24671eccbf4a404542597b
David Holm <dholmster@gmail.com>
2013-07-12 16:54:11 -0700

    Recipes for pretty-mode-plus and smart-forward

--- .emacs.d/ux/pretty-mode-plus.el
+++ .emacs.d/ux/pretty-mode-plus.el
@@ -1,5 +1,7 @@
 ;;; (Initialization) ;;;
 (require-package (:name pretty-mode-plus
+			:type elpa
+			:repo ("marmalade" . "http://marmalade-repo.org/packages/")
                         :after (dholm/pretty-mode-plus-init)))
 
 (defun dholm/pretty-mode-plus-init ()
--- .emacs.d/ux/smart-forward.el
+++ .emacs.d/ux/smart-forward.el
@@ -1,5 +1,8 @@
 ;;; (Initialization) ;;;
-(require-package (:name smart-forward))
+(require-package (:name smart-forward
+			:type elpa
+			:repo ("melpa" . "http://melpa.milkbox.net/packages/")
+			:depends (expand-region)))
 
 
 (provide 'ux/smart-forward)
