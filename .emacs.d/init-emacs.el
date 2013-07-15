65b28a5504b9fef0056dcb57b014ae4f2ab2908a
David Holm <david.holm@ericsson.com>
2013-07-16 00:58:01 +0200

    Silently delete old backups

--- .emacs.d/init-emacs.el
+++ .emacs.d/init-emacs.el
@@ -19,7 +19,16 @@
  auto-save-file-name-transforms `((".*" ,(concat emacs-autosaves-directory "/\\1") t))
  backup-directory-alist `((".*" . ,(path-join *user-cache-directory* "backups")))
  ;; Put session backups into the cache directory
- auto-save-list-file-prefix (path-join *user-cache-directory* "auto-save-list" ".saves-"))
+ auto-save-list-file-prefix (path-join *user-cache-directory* "auto-save-list" ".saves-")
+ ;; Version-control backup files
+ version-control t
+ ;; Keep 16 new versions and 2 old versions
+ kept-new-versions 16
+ kept-old-versions 2
+ ;; Delete old versions without asking
+ delete-old-versions t
+ ;; Copy linked files instead of renaming
+ backup-by-copying-when-linked t)
 
 
 (provide 'init-emacs)
--- .emacs.d/ux/session.el
+++ .emacs.d/ux/session.el
@@ -1,27 +1,36 @@
 ;;; (Initialization) ;;;
-(require-package (:name session))
+(require-package (:name session
+                        :before (dholm/session-before-init)
+                        :after (dholm/session-init)))
 
 
-(setq session-save-file (path-join *user-cache-directory* "session")
-      desktop-globals-to-save
-      (append '((extended-command-history . 30)
-                (file-name-history        . 100)
-                (ido-last-directory-list  . 100)
-                (ido-work-directory-list  . 100)
-                (ido-work-file-list       . 100)
-                (grep-history             . 30)
-                (compile-history          . 30)
-                (minibuffer-history       . 50)
-                (query-replace-history    . 60)
-                (read-expression-history  . 60)
-                (regexp-history           . 60)
-                (regexp-search-ring       . 20)
-                (search-ring              . 20)
-                (comint-input-ring        . 50)
-                (shell-command-history    . 50)
-                desktop-missing-file-warning
-                tags-file-name
-                register-alist)))
+(defun dholm/session-before-init ()
+  (custom-set-variables '(session-use-package t nil (session))))
+
+
+(defun dholm/session-init ()
+  (setq-default
+   session-save-file (path-join *user-cache-directory* "session")
+   desktop-globals-to-save
+   (append '((extended-command-history . 30)
+             (file-name-history        . 100)
+             (ido-last-directory-list  . 100)
+             (ido-work-directory-list  . 100)
+             (ido-work-file-list       . 100)
+             (grep-history             . 30)
+             (compile-history          . 30)
+             (minibuffer-history       . 50)
+             (query-replace-history    . 60)
+             (read-expression-history  . 60)
+             (regexp-history           . 60)
+             (regexp-search-ring       . 20)
+             (search-ring              . 20)
+             (comint-input-ring        . 50)
+             (shell-command-history    . 50)
+             desktop-missing-file-warning
+             tags-file-name
+             register-alist))))
+
 
 (add-hook 'after-init-hook 'session-initialize)
 
