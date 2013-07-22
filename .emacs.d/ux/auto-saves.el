cd77d368cb1a092f26c638d90516275c1356ac62
David Holm <david.holm@ericsson.com>
2013-07-22 15:26:15 -0700

    Use a constant for user backup directory

--- .emacs.d/ux/auto-saves.el
+++ .emacs.d/ux/auto-saves.el
@@ -1,5 +1,6 @@
 ;; Set up the autosaves directory
 (defconst *user-autosaves-directory* (path-join *user-cache-directory* "autosaves"))
+(defconst *user-backups-directory* (path-join *user-cache-directory* "backups"))
 
 ;; Emacs will create the backup dir automatically, but not the autosaves dir
 (make-directory *user-autosaves-directory* t)
@@ -8,10 +9,9 @@
 (setq
  ;; Put autosave files (ie #foo#) and backup files (ie foo~) into a cache dir
  auto-save-file-name-transforms `((".*" ,(concat *user-autosaves-directory* "/\\1") t))
- backup-directory-alist `((".*" . ,(path-join *user-cache-directory* "backups")))
+ backup-directory-alist `((".*" . ,*user-backups-directory*))
  ;; Put session backups into the cache directory
  auto-save-list-file-prefix (path-join *user-cache-directory* "auto-save-list" ".saves-")
-
  ;; Version-control backup files
  version-control t
  ;; Keep 16 new versions and 2 old versions
