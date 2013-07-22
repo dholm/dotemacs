ffaf51ded980c2c10c9336c99f2638fcc136694c
David Holm <dholmster@gmail.com>
2013-07-22 09:22:40 -0700

    Move xcscope from submodule to el-get

--- .emacs.d/utilities/xcscope.el
+++ .emacs.d/utilities/xcscope.el
@@ -1,5 +1,16 @@
-(push (path-join user-emacs-directory "utilities" "xcscope") load-path)
-(require 'xcscope)
+;;; xcscope -- cscope integration
+;;; Commentary:
+;;; Code:
+
+(require-package (:name xcscope
+			:type github
+			:pkgname "dholm/xcscope"
+			:features xcscope
+			:prepare (progn
+				   (setq cscope-indexing-script
+					 (expand-file-name
+					  (concat (el-get-package-directory "xcscope") "cscope-indexer"))))))
 
 
 (provide 'utilities/xcscope)
+;;; xcscope.el ends here
