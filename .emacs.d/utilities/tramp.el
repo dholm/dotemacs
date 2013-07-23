c6b92e90129b3cab20a14f4612ceae0222260c96
David Holm <dholmster@gmail.com>
2013-07-23 09:58:06 -0700

    Move tramp cache to cache directory

---
+++ .emacs.d/utilities/tramp.el
@@ -0,0 +1,10 @@
+;;; tramp --- remote file access
+;;; Commentary:
+;;; Code:
+
+(setq
+ tramp-persistency-file-name (path-join *user-cache-directory* "tramp"))
+
+
+(provide 'utilities/tramp)
+;;; tramp.el ends here
