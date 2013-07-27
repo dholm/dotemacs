f2382f944126652120bfaa84ef3c19950dc0756f
David Holm <dholmster@gmail.com>
2013-07-26 17:13:06 -0700

    Add functionality to show benchmarks in a buffer sorted by time

--- .emacs.d/lib/benchmarking.el
+++ .emacs.d/lib/benchmarking.el
@@ -4,17 +4,37 @@
 ;;; Code:
 
 (defun dholm/time-subtract-millis (b a)
+  "Calculate the number of milliseconds that have elapsed between B and A."
   (* 1000.0 (float-time (time-subtract b a))))
 
 
-(defvar dholm/require-times nil
-    "A list of (FEATURE . LOAD-DURATION).
+(defvar dholm/require-times (make-hash-table :test 'equal)
+    "A hash table of (FEATURE . LOAD-DURATION).
 LOAD-DURATION is the time taken in milliseconds to load FEATURE.")
 
 
-(defvar dholm/load-times nil
-    "A list of (FILE . LOAD-DURATION).
+(defvar dholm/load-times (make-hash-table :test 'equal)
+    "A hash-table of (FILE . LOAD-DURATION).
 LOAD-DURATION is the time taken in milliseconds to load FILE.")
+
+
+(defun dholm/show-require-times ()
+  "Show the benchmark for require."
+  (interactive)
+  (dholm/show-times dholm/require-times "*require times*"))
+
+(defun dholm/show-load-times ()
+  "Show the benchmark for load."
+  (interactive)
+  (dholm/show-times dholm/load-times "*load times*"))
+
+
+(defmacro dholm/show-times (hash-table name)
+  "Show the benchmark for the specified HASH-TABLE in buffer NAME."
+  `(with-output-to-temp-buffer ,name
+     (do-hash-table-sorted-by-value (val ,hash-table)
+       (princ (format "%s %d\n" val (gethash val ,hash-table))))
+     (switch-to-buffer ,name)))
 
 
 (defadvice require
@@ -25,11 +45,9 @@ LOAD-DURATION is the time taken in milliseconds to load FILE.")
     (prog1
         ad-do-it
       (when (and (not already-loaded) (memq feature features))
-        (add-to-list 'dholm/require-times
-                     (cons feature
-                           (dholm/time-subtract-millis (current-time)
-                                                       require-start-time))
-                     t)))))
+        (puthash feature (dholm/time-subtract-millis (current-time)
+                                                     require-start-time)
+                 dholm/require-times)))))
 
 
 (defadvice load
@@ -38,11 +56,12 @@ LOAD-DURATION is the time taken in milliseconds to load FILE.")
   (let* ((load-start-time (current-time)))
     (prog1
         ad-do-it
-      (add-to-list 'dholm/load-times
-                     (cons file
-                           (dholm/time-subtract-millis (current-time)
-                                                       load-start-time))
-                     t))))
+      (progn
+        (unless (eq (gethash file dholm/load-times) nil)
+          (message (format "Loading %s which has already been loaded!" file)))
+        (puthash file (dholm/time-subtract-millis (current-time)
+                                                  load-start-time)
+                 dholm/load-times)))))
 
 
 (provide 'lib/benchmarking)
