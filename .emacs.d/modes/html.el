9637a425e5878014328bde3d41768b8f019a862e
David Holm <dholmster@gmail.com>
2013-07-31 16:10:13 -0700

    Add auto-completion in XML mode

--- .emacs.d/modes/html.el
+++ .emacs.d/modes/html.el
@@ -1,11 +1,9 @@
-;;; html --- initializes HTML modes
+;;; html.el --- initializes HTML modes
 ;;; Commentary:
 ;;; Code:
 
 (defun dholm/html-mode-hook ()
-  "HTML mode hook."
-  ;; Configure nxml auto completion
-  (setq nxml-slash-auto-complete-flag t))
+  "HTML mode hook.")
 
 (defun guess-xhtml-hook ()
   "Guess whether the current buffer is XHTML."
@@ -14,41 +12,46 @@
         (search-forward-regexp "<[?]xml\\|//W3C//DTD XHTML" 80 t))
     (html-mode)))
 
-(add-hook 'html-mode-hook 'guess-xhtml-hook t)
-(add-hook 'html-mode-hook 'dholm/html-mode-hook)
+(defun dholm/html-mode-init ()
+  "Initialize HTML mode."
+  ;; Configure mmm for HTML
+  (after-load 'mmm
+    (mmm-add-group
+     'fancy-html
+     '((html-php-tagged
+        :submode php-mode
+        :face mmm-code-submode-face
+        :front "<[?]php"
+        :back "[?]>")
+       (html-css-embedded
+        :submode css-mode
+        :face mmm-declaration-submode-face
+        :front "\]*>"
+        :back "")
+       (html-css-attribute
+        :submode css-mode
+        :face mmm-declaration-submode-face
+        :front "\\bstyle=\\s-*\""
+        :back "\"")
+       (html-javascript-attribute
+        :submode javascript-generic-mode
+        :face mmm-code-submode-face
+        :front "\\bon\\w+=\\s-*\""
+        :back "\"")))
 
-(after-load 'mmm
-  (mmm-add-group
-   'fancy-html
-   '((html-php-tagged
-      :submode php-mode
-      :face mmm-code-submode-face
-      :front "<[?]php"
-      :back "[?]>")
-     (html-css-embedded
-      :submode css-mode
-      :face mmm-declaration-submode-face
-      :front "\]*>"
-      :back "")
-     (html-css-attribute
-      :submode css-mode
-      :face mmm-declaration-submode-face
-      :front "\\bstyle=\\s-*\""
-      :back "\"")
-     (html-javascript-attribute
-      :submode javascript-generic-mode
-      :face mmm-code-submode-face
-      :front "\\bon\\w+=\\s-*\""
-      :back "\"")))
+    ;; Register file extensions for html-mode
+    (add-auto-mode 'html-mode
+                   "\\.inc$" "\\.php[34]?$" "\\.[sj]?html?$")
 
-  ;; Register file extensions for html-mode
-  (add-auto-mode 'html-mode
-                 "\\.inc$" "\\.php[34]?$" "\\.[sj]?html?$")
+    ;; Enable support for mmm extensions
+    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil html-js))
+    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil embedded-css))
+    (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil fancy-html)))
 
-  ;; Enable support for mmm extensions
-  (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil html-js))
-  (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil embedded-css))
-  (add-to-list 'mmm-mode-ext-classes-alist '(html-mode nil fancy-html)))
+  (add-hook 'html-mode-hook 'guess-xhtml-hook t)
+  (add-hook 'html-mode-hook 'dholm/html-mode-hook))
+
+(dholm/html-mode-init)
 
 
 (require-package '(:name tidy))
--- .emacs.d/modes/xml.el
+++ .emacs.d/modes/xml.el
@@ -1,23 +1,30 @@
-;;; xml --- initializes XML modes
+;;; xml.el --- initializes XML modes
 ;;; Commentary:
 ;;; Code:
 
-(defun dholm/xml-mode-hook ()
+(defun dholm/nxml-mode-hook ()
   "XML mode hook."
-  ;; Use nxml-mode for XML
-  (nxml-mode t)
   ;; Configure nxml auto completion
+  (require 'auto-complete-nxml)
   (setq nxml-slash-auto-complete-flag t))
 
 
 (defun dholm/xml-mode-init ()
   "Initialize xml mode."
-  (add-hook 'xml-mode-hook 'dholm/xml-mode-hook)
+  ;; Use nxml-mode for XML
+  (fset 'xml-mode 'nxml-mode)
+  (add-hook 'nxml-mode-hook 'dholm/nxml-mode-hook)
   (setq
    ;; Enable magic-mode to detect XML
    magic-mode-alist (cons '("<\\?xml " . nxml-mode) magic-mode-alist)))
 
 (dholm/xml-mode-init)
+
+
+(defun dholm/auto-complete-nxml-init ()
+  "Initialize auto completion for nxml mode.")
+
+(require-package '(:name auto-complete-nxml :after (dholm/auto-complete-nxml-init)))
 
 
 (provide 'modes/xml)
