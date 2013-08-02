d9e4c4c0fc6d9a397e23f52700d9abc7ff131727
David Holm <dholmster@gmail.com>
2013-08-02 10:47:19 -0700

    Configure deferred-flyspell correctly

--- .emacs.d/utilities/flyspell.el
+++ .emacs.d/utilities/flyspell.el
@@ -1,11 +1,22 @@
-;;; flyspell --- spell checking on the fly
+;;; flyspell.el --- spell checking on the fly
 ;;; Commentary:
 ;;; Code:
 
+(defconst *has-aspell* (executable-find "aspell"))
+
+(defun user/flyspell-mode-hook ()
+  "Hook for fly spell mode."
+  (deferred-flyspell:install-hooks))
+
 (defun user/flyspell-init ()
-  "Initialize flyspell."
-  ;;; (Faces) ;;;
-  )
+  "Initialize fly spell."
+  (when *has-aspell*
+    (setq-default
+     ispell-program-name "aspell"
+     ispell-list-command "--list"
+     ispell-extra-args '("--sug-mode=ultra")))
+
+  (add-hook 'flyspell-mode-hook 'user/flyspell-mode-hook))
 
 (after-load 'flyspell
   (user/flyspell-init))
