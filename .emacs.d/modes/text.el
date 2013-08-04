605c30010321261dcc0cefb7669f990595b6e8c5
David Holm <dholmster@gmail.com>
2013-08-04 15:34:25 -0700

    Do not use before-save-hook
    before-save-hook is global, use a per-mode hook instead.

--- .emacs.d/modes/text.el
+++ .emacs.d/modes/text.el
@@ -10,7 +10,7 @@
   ;; Run spell-checker
   (flyspell-mode)
   ;; Delete trailing whitespace on save
-  (add-hook 'before-save-hook 'delete-trailing-whitespace nil t)
+  (add-hook 'write-contents-functions 'delete-trailing-whitespace nil t)
   ;; Enable dtrt-indent to attempt to identify the indentation rules used
   (after-load 'dtrt-indent
     (dtrt-indent-mode t)))
