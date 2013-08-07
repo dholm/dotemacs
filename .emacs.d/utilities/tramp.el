d937f76efb277c60f3bde25100c1c792f666191c
David Holm <dholmster@gmail.com>
2013-08-06 17:58:14 -0700

    Configure tramp to parse system and user configuration files

--- .emacs.d/utilities/tramp.el
+++ .emacs.d/utilities/tramp.el
@@ -2,8 +2,21 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq
- tramp-persistency-file-name (path-join *user-cache-directory* "tramp"))
+(defun user/tramp-init ()
+  "Initialize tramp."
+  (setq-default
+   tramp-default-method "ssh"
+   tramp-persistency-file-name (path-join *user-cache-directory* "tramp"))
+
+  ;; Load SSH configuration
+  (tramp-set-completion-function "ssh"
+                                 `((tramp-parse-sconfig
+                                    "/etc/ssh/ssh_config")
+                                   (tramp-parse-sconfig
+                                    ,(path-join *user-home-directory* ".ssh" "config")))))
+
+(after-load 'tramp
+  (user/tramp-init))
 
 
 (provide 'utilities/tramp)
