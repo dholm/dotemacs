618689b1287300bf95fe7c497c619afcf0925195
David Holm <dholmster@gmail.com>
2013-08-14 16:27:58 -0700

    Define prefix bindings as custom so they can be locally redefined

--- .emacs.d/init-bindings.el
+++ .emacs.d/init-bindings.el
@@ -3,24 +3,52 @@
 ;;; Code:
 
 ;; Set up prefixes for groups of commands
+(defcustom user/navigation-keyboard-prefix (kbd "C-c n")
+  "Keyboard prefix to use for navigation commands."
+  :type 'key-sequence
+  :group 'user)
+
+(defcustom user/documentation-keyboard-prefix (kbd "C-c d")
+  "Keyboard prefix to use for documentation commands."
+  :type 'key-sequence
+  :group 'user)
+
+(defcustom user/code-keyboard-prefix (kbd "C-c c")
+  "Keyboard prefix to use for code manipulation commands."
+  :type 'key-sequence
+  :group 'user)
+
+(defcustom user/vcs-keyboard-prefix (kbd "C-c v")
+  "Keyboard prefix to use for version control commands."
+  :type 'key-sequence
+  :group 'user)
+
+(defcustom user/utilities-keyboard-prefix (kbd "C-c u")
+  "Keyboard prefix to use for utility commands."
+  :type 'key-sequence
+  :group 'user)
+
 (define-prefix-command 'user/navigation-map)
-(define-prefix-command 'user/code-map)
+(define-key global-map user/navigation-keyboard-prefix 'user/navigation-map)
+
 (define-prefix-command 'user/documentation-map)
+(define-key global-map user/documentation-keyboard-prefix 'user/documentation-map)
+
+(define-prefix-command 'user/code-map)
+(define-key global-map user/code-keyboard-prefix 'user/code-map)
+
 (define-prefix-command 'user/vcs-map)
+(define-key global-map user/vcs-keyboard-prefix 'user/vcs-map)
+
 (define-prefix-command 'user/utilities-map)
-
-(global-set-key (kbd "C-c n") 'user/navigation-map)
-(global-set-key (kbd "C-c c") 'user/code-map)
-(global-set-key (kbd "C-c d") 'user/documentation-map)
-(global-set-key (kbd "C-c v") 'user/vcs-map)
-(global-set-key (kbd "C-c u") 'user/utilities-map)
+(define-key global-map user/utilities-keyboard-prefix 'user/utilities-map)
 
 
-;; Bookmarks
+;; Bind bookmarks to C-c b
 (global-set-key (kbd "C-c b") 'bookmark-map)
 
 
-;; Binds goto-line to navigation command g which is easier to access
+;; Binds goto-line to navigation command g which is easier to access than M-g g
 (define-key user/navigation-map (kbd "g") 'goto-line)
 
 
