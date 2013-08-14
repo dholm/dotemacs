e57f235ffd1e01a5c54ae8ceab2618a3b1808e9f
David Holm <dholmster@gmail.com>
2013-08-14 09:25:18 -0700

    Bind C-c n s to jump to any symbol when in CEDET mode

--- .emacs.d/utilities/cedet.el
+++ .emacs.d/utilities/cedet.el
@@ -18,6 +18,7 @@
   (define-key user/code-map (kbd "=") 'semantic-decoration-include-visit)
 
   (define-key user/navigation-map (kbd "j") 'semantic-ia-fast-jump)
+  (define-key user/navigation-map (kbd "s") 'semantic-complete-jump)
   (define-key user/navigation-map (kbd "b") 'semantic-mrub-switch-tags)
   (define-key user/navigation-map (kbd "p") 'semantic-analyze-proto-impl-toggle)
   (define-key user/navigation-map (kbd "r") 'semantic-symref)
