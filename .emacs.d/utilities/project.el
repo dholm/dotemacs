90ca43d6e72b75833fbac420e3a8044dbced3703
David Holm <dholmster@gmail.com>
2013-08-23 15:10:07 -0700

    Bugfix in function to locate project root (support for symbolic links)

--- .emacs.d/utilities/project.el
+++ .emacs.d/utilities/project.el
@@ -11,24 +11,27 @@
 
 (defun user/ffip-project-root (path)
   "Use find-file-in-project to locate root from PATH."
-  (let ((path-directory (file-name-directory path)))
-    (or ffip-project-root
-       (if (functionp ffip-project-root-function)
-           (funcall ffip-project-root-function)
-         (if (listp ffip-project-file)
-             (some (apply-partially 'locate-dominating-file
-                                    path-directory)
-                   ffip-project-file)
-           (locate-dominating-file path-directory
-                                   ffip-project-file))))))
+  (when (el-get-package-is-installed 'find-file-in-project)
+    (require 'find-file-in-project)
+    (let ((path-directory (file-name-directory path)))
+      (or ffip-project-root
+         (if (functionp ffip-project-root-function)
+             (funcall ffip-project-root-function)
+           (if (listp ffip-project-file)
+               (some (apply-partially 'locate-dominating-file
+                                      path-directory)
+                     ffip-project-file)
+             (locate-dominating-file path-directory
+                                     ffip-project-file)))))))
 
 
 (defun user/project-root (path)
   "Get the project root for PATH, if it exists."
-  (let ((ede-proj (user/ede-project path)))
+  (let* ((path (file-truename path))
+         (ede-proj (user/ede-project path)))
     (cond
      (ede-proj (ede-project-root-directory ede-proj))
-     ((featurep 'find-file-in-project) (user/ffip-project-root path)))))
+     (t (user/ffip-project-root path)))))
 
 
 (defun user/gnu-global-tags-location (path)
