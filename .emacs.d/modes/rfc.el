2cb874245e054d70a53a624220f7cd3f349cf9b2
David Holm <dholmster@gmail.com>
2013-09-27 10:16:32 -0700

    RFC mode support

---
+++ .emacs.d/modes/rfc.el
@@ -0,0 +1,28 @@
+;;; rfc.el --- RFC mode support
+;;; Commentary:
+;;; Code:
+
+(defun user/rfc-mode-hook ()
+  "RFC mode hook.")
+
+
+(defun user/irfc-init ()
+  "Initialize irfc."
+  (setq-default
+   ;; Put RFCs in user cache directory.
+   irfc-directory (path-join *user-cache-directory* "rfcs")
+   ;; Load irfc mode automatically.
+   irfc-assoc-mode t)
+
+  ;; Create cache directory for irfc.
+  (make-directory irfc-directory t)
+
+  (add-hook 'irfc-mode-hook 'user/rfc-mode-hook)
+  (add-auto-mode 'irfc-mode "/rfc[0-9]+\\.txt\\'"))
+
+
+(require-package '(:name irfc :after (user/irfc-init)))
+
+
+(provide 'modes/rfc)
+;;; rfc.el ends here
