7ba2bff016ad2880b017cd2d2b01ddb6ec57feea
David Holm <dholmster@gmail.com>
2013-09-27 12:06:16 -0700

    Clean up tabs/spaces when saving buffer in prog-mode

--- .emacs.d/modes/prog.el
+++ .emacs.d/modes/prog.el
@@ -1,10 +1,10 @@
-;;; prog --- setup shared defaults for programming modes
+;;; prog.el --- setup shared defaults for programming modes
 ;;; Commentary:
 ;;; Code:
 
 (defun user/prog-mode-hook ()
   "Programming mode hook."
-  (setq-default
+  (setq
    ;; Indent using spaces by default
    indent-tabs-mode nil
    ;; When using fill-paragraph or auto-fill-mode break lines at 80 characters by
@@ -20,7 +20,7 @@
   ;; Run spell-checker
   (flyspell-prog-mode)
   ;; Delete trailing whitespace on save
-  (add-hook 'write-contents-functions 'delete-trailing-whitespace nil t)
+  (add-hook 'write-contents-functions 'user/prog-mode-buffer-cleanup nil t)
   ;; Enable dtrt-indent to attempt to identify the indentation rules used
   (after-load 'dtrt-indent
     (dtrt-indent-mode t))
@@ -32,6 +32,15 @@
     (diminish 'abbrev-mode))
   ;; Automatically indent on newlines
   (local-set-key (kbd "RET") 'newline-and-indent))
+
+
+(defun user/prog-mode-buffer-cleanup ()
+  "Cleans up the buffer contents."
+  (interactive)
+  (delete-trailing-whitespace)
+  (if indent-tabs-mode
+      (untabify (point-min) (point-max))
+    (tabify (point-min) (point-max))))
 
 
 (defun user/rainbow-delimiters-init ()
