1a380f7ba72a6a5fd34ea42c4a24c11b93592cf6
David Holm <dholmster@gmail.com>
2013-09-27 14:09:25 -0700

    Improved perspective configuration

--- .emacs.d/utilities/perspective.el
+++ .emacs.d/utilities/perspective.el
@@ -1,10 +1,19 @@
-;;; perspective --- perspectives in Emacs
+;;; perspective.el --- perspectives in Emacs
 ;;; Commentary:
 ;;; Code:
 
 (defun user/perspective-init ()
   "Initialize perspective."
-  (define-key user/utilities-map (kbd "p") 'persp-mode))
+  (setq-default
+   ;; Always show perspective in the modeline.
+   persp-show-modestring t
+   frame-title-format '("Emacs: %b [" (:eval (persp-name persp-curr)) "]"))
+
+  ;;; (Bindings) ;;;
+  (define-key user/utilities-map (kbd "p") 'persp-mode)
+  (after-load 'perspective
+    (define-key persp-mode-map (kbd "C-c p") perspective-map)
+    (define-key persp-mode-map (kbd "C-c p SPC") 'persp-switch-quick)))
 
 (require-package '(:name perspective :after (user/perspective-init)))
 
