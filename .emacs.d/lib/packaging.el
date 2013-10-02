33d10a2281c8483f1cb9c0d19801a772cffe7098
David Holm <dholmster@gmail.com>
2013-10-02 15:02:18 -0700

    Optimized el-get startup

--- .emacs.d/lib/packaging.el
+++ .emacs.d/lib/packaging.el
@@ -11,8 +11,12 @@
 
 
 ;; Configure and load el-get
+(defvar el-get-sources nil "List of package definitions for el-get.")
+(defvar el-get-safe-mode nil "Start el-get in safe mode.")
+
 (add-to-list 'load-path (path-join *user-el-get-directory* "el-get"))
 (unless (require 'el-get nil 'noerror)
+  (setq el-get-safe-mode t)
   (with-current-buffer
       (url-retrieve-synchronously
        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
@@ -21,13 +25,10 @@
       (eval-print-last-sexp))))
 
 
-(defvar el-get-packages nil "List of el-get packages to sync.")
-(defvar el-get-sources nil "List of package definitions for el-get.")
-
 (when (featurep 'el-get)
   (setq-default
    el-get-user-package-directory (path-join user-emacs-directory "init")
-   el-get-verbose t))
+   el-get-verbose el-get-safe-mode))
 
 
 (defun require-package (package)
@@ -37,13 +38,15 @@
 
 (defun user/package-list ()
   "Get the list of registered packages."
-  (append el-get-packages
-          (mapcar 'el-get-as-symbol (mapcar 'el-get-source-name el-get-sources))))
+  (mapcar 'el-get-as-symbol (mapcar 'el-get-source-name el-get-sources)))
 
 
 (defun user/sync-packages ()
   "Sync all required packages."
-  (el-get 'sync (user/package-list)))
+  (let ((package-list (user/package-list)))
+    (if el-get-safe-mode
+        (el-get 'sync package-list)
+      (el-get nil package-list))))
 
 
 ;; Make sure el-get is registered so that el-get-cleanup doesn't remove it
