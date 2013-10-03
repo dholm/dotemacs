1f682c39c1951cc0fad792b33f3d214acdc57d74
David Holm <dholmster@gmail.com>
2013-10-03 09:33:33 -0700

    Put frame bindings where they belong

--- .emacs.d/init-bindings.el
+++ .emacs.d/init-bindings.el
@@ -68,12 +68,5 @@
 (global-set-key (kbd "C-x m") 'execute-extended-command)
 
 
-;; Resizing windows
-(global-set-key (kbd "C-c <up>") 'shrink-window)
-(global-set-key (kbd "C-c <down>") 'enlarge-window)
-(global-set-key (kbd "C-c <left>") 'shrink-window-horizontally)
-(global-set-key (kbd "C-c <right>") 'enlarge-window-horizontally)
-
-
 (provide 'init-bindings)
 ;;; init-bindings.el ends here
--- .emacs.d/ux/frames.el
+++ .emacs.d/ux/frames.el
@@ -15,7 +15,13 @@
   ;; Remove all the mouse-assisted frames
   (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
   (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-  (if (fboundp 'menu-bar-mode) (menu-bar-mode -1)))
+  (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+
+  ;;; (Bindings) ;;;
+  (global-set-key (kbd "C-c <up>") 'shrink-window)
+  (global-set-key (kbd "C-c <down>") 'enlarge-window)
+  (global-set-key (kbd "C-c <left>") 'shrink-window-horizontally)
+  (global-set-key (kbd "C-c <right>") 'enlarge-window-horizontally))
 
 
 (user/frames-init)
