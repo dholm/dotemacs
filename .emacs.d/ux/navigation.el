8e049b5714e9d1ffdc9d9d58b8ffd691b2aa125c
David Holm <dholmster@gmail.com>
2013-11-10 12:22:41 -0800

    Enable mouse support in iTerm2

--- .emacs.d/ux/navigation.el
+++ .emacs.d/ux/navigation.el
@@ -7,6 +7,12 @@
   (require-package '(:name ace-jump-mode :after (user/ace-jump-mode-init)))
   (require-package '(:name smart-forward :after (user/smart-forward-init)))
 
+  ;; Enable mouse in iTerm2
+  (when (eq system-type 'darwin)
+    (require 'mouse)
+    (xterm-mouse-mode t)
+    (defun track-mouse (e)))
+
   ;;; (Bindings) ;;;
   ;; Binds goto-line to navigation command g which is easier to access than M-g g
   (define-key user/navigation-map (kbd "g") 'goto-line))
