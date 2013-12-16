03307d8b2aaa1c57c2a395fa70217a2a10472ab6
David Holm <dholmster@gmail.com>
2013-12-16 14:27:48 -0800

    Add word to dictionary using code + 'S'

--- .emacs.d/utilities/flyspell.el
+++ .emacs.d/utilities/flyspell.el
@@ -5,7 +5,8 @@
 (defun user/flyspell-mode-hook ()
   "Hook for fly spell mode."
   ;;; (Bindings) ;;;
-  (define-key user/code-map (kbd "s") 'ispell-word))
+  (define-key user/code-map (kbd "s") 'ispell-word)
+  (define-key user/code-map (kbd "S") 'user/flyspell-add-word-to-dict))
 
 
 (defun user/flyspell-lazy-init ()
@@ -28,7 +29,24 @@
                            :after (user/flyspell-lazy-init)))
   (require-package '(:name auto-dictionary))
 
-  (add-hook 'flyspell-mode-hook 'user/flyspell-mode-hook))
+  (add-hook 'flyspell-mode-hook 'user/flyspell-mode-hook)
+
+  ;;; (Functions) ;;;
+  (defun user/flyspell-add-word-to-dict ()
+    "Add the word at the current location to the private dictionary without question."
+    (interactive)
+    ;; Use the correct dictionary.
+    (flyspell-accept-buffer-local-defs)
+    (setq opoint (point-marker))
+    (let ((cursor-location (point))
+          (word (flyspell-get-word nil)))
+      (if (consp word)
+          (let ((start (car (cdr word)))
+                (end (car (cdr (cdr word))))
+                (word (car word)))
+            ;; The word is incorrect, we have to propose a replacement.
+            (flyspell-do-correct 'save nil word cursor-location start end opoint)))
+      (ispell-pdict-save t))))
 
 (user/flyspell-init)
 
