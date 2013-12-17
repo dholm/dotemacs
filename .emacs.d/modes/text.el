a8b28356e86be21426f75abcaea13a4148031899
David Holm <dholmster@gmail.com>
2013-12-16 17:22:50 -0800

    Thesaurus support using Synosaurus

--- .emacs.d/modes/text.el
+++ .emacs.d/modes/text.el
@@ -25,12 +25,34 @@
     (dtrt-indent-mode t))
 
   ;;; (Bindings) ;;;
-  (define-key user/code-map (kbd "f") 'fill-paragraph))
+  (define-key user/code-map (kbd "f") 'fill-paragraph)
+  (when (el-get-package-is-installed 'synosaurus)
+    (define-key user/code-map (kbd "t") 'synosaurus-choose-and-replace)))
+
+
+(defun user/synosaurus-init ()
+  "Initialize Synosaurus thesaurus."
+  (setq-default
+   ;; Use popup to display options.
+   synosaurus-choose-method 'popup
+   ;; Use WordNet for lookups.
+   synosaurus-lookup-function 'wordnet-lookup))
 
 
 (defun user/text-mode-init ()
   "Initialize generic text editing mode."
-  (add-hook 'text-mode-hook 'user/text-mode-hook))
+  (add-hook 'text-mode-hook 'user/text-mode-hook)
+
+  ;;; (Packages) ;;;
+  (require-package '(:name synosaurus
+                           :after (user/synosaurus-init)
+                           :type github
+                           :pkgname "rootzlevel/synosaurus"
+                           :prepare (progn
+                                      (autoload 'synosaurus-lookup "synosaurus")
+                                      (autoload 'synosaurus-choose-and-replace "synosaurus")
+                                      (autoload 'openthesaurus-lookup "synosaurus-openthesaurus")
+                                      (autoload 'wordnet-lookup "synosaurus-wordnet")))))
 
 
 (user/text-mode-init)
