3e2489f08394eb22cda3773129b762d9e375028d
David Holm <dholmster@gmail.com>
2013-12-30 16:14:33 +0100

    Verilog mode support

---
+++ .emacs.d/modes/verilog.el
@@ -0,0 +1,20 @@
+;;; verilog.el --- Verilog mode support
+;;; Commentary:
+;;; Code:
+
+(defun user/verilog-mode-hook ()
+  "Verilog mode hook.")
+
+
+(defun user/verilog-mode-init ()
+  "Initialize Verilog mode."
+  (require-package '(:name verilog-mode))
+  (require-package '(:name auto-complete-verilog))
+
+  (add-hook 'verilog-mode-hook 'user/verilog-mode-hook))
+
+(user/verilog-mode-init)
+
+
+(provide 'modes/verilog)
+;;; verilog.el ends here
