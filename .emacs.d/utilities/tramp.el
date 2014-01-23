7944631fa7e47514976de269e023f285ef0e069d
David Holm <dholmster@gmail.com>
2014-01-23 21:29:05 +0100

    Improve tramp configuration

--- .emacs.d/utilities/tramp.el
+++ .emacs.d/utilities/tramp.el
@@ -5,18 +5,29 @@
 (defun user/tramp-init ()
   "Initialize tramp."
   (setq-default
+   ;; Default file transfer method.
    tramp-default-method "ssh"
-   tramp-persistency-file-name (path-join *user-cache-directory* "tramp"))
+   ;; Cache passwords.
+   password-cache t
+   password-cache-expiry 1000
+   ;; Persistency files.
+   tramp-persistency-file-name (path-join *user-cache-directory* "tramp")
+   ;; Auto save storage.
+   tramp-auto-save-directory (path-join *user-cache-directory* "tramp"
+                                        "auto-saves"))
 
   ;; Load SSH configuration
-  (tramp-set-completion-function "ssh"
-                                 `((tramp-parse-sconfig
-                                    "/etc/ssh/ssh_config")
-                                   (tramp-parse-sconfig
-                                    ,(path-join *user-home-directory* ".ssh" "config")))))
+  (after-load 'dash
+    (tramp-set-completion-function
+     "ssh" (-map
+            (lambda (x) (list 'tramp-parse-config x))
+            (-remove
+             (lambda (x) (not (file-exists-p x)))
+             `(,(path-join "/" "etc" "ssh_config")
+               ,(path-join "/" "etc" "ssh" "ssh_config")
+               ,(path-join *user-home-directory* ".ssh" "config")))))))
 
-(after-load 'tramp
-  (user/tramp-init))
+(require-package '(:name tramp :after (user/tramp-init)))
 
 
 (provide 'utilities/tramp)
