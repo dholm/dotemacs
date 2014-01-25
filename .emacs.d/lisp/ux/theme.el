0f204383627e41abaa3329d52a70ae3f644a8577
David Holm <dholmster@gmail.com>
2014-01-25 11:33:18 +0100

    Set font by configuring default face attribute

--- .emacs.d/lisp/ux/theme.el
+++ .emacs.d/lisp/ux/theme.el
@@ -2,17 +2,22 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun user/solarized-init ()
+  "Initialize Solarized theme."
+  (load-theme 'solarized t))
+
+
 (defun user/theme-init ()
   "Initialize Emacs theme."
-  ;; Make comments stand out.
-  (set-face-foreground 'font-lock-comment-face "red")
-  (set-face-foreground 'font-lock-comment-delimiter-face "red")
-
-  ;; Font when in graphics mode.
   (when (display-graphic-p)
-    (if (eq system-type 'windows-nt)
-        (set-default-font "consolas-10")
-      (set-default-font "Meslo LG S DZ:pixelsize=10:foundry=bitstream:weight=normal:slant=normal:width=normal:spacing=100:scalable=true")))
+    (cond
+         ((eq system-type 'darwin)
+          (set-face-attribute 'default nil :family "Menlo" :height 110 :weight 'normal))
+         ((eq system-type 'windows-nt)
+          (set-face-attribute 'default nil :family "Consolas" :height 100 :weight 'normal))
+         ((eq system-type 'gnu/linux)
+          (set-face-attribute 'default nil :foundry "bitstream" :family "Meslo LG S DZ"
+                                         :height 100 :weight 'normal))))
 
   ;; Enable blinking cursor
   (blink-cursor-mode)
@@ -22,12 +27,6 @@
                            :pkgname "dholm/solarized-theme"
                            :prepare (add-to-list 'custom-theme-load-path default-directory)
                            :after (user/solarized-init))))
-
-
-(defun user/solarized-init ()
-  "Initialize Solarized theme."
-  (load-theme 'solarized t))
-
 
 (user/theme-init)
 
