1f55cb668730a93f34b301b52a9b9e4828e19161
David Holm <dholmster@gmail.com>
2014-01-26 10:52:33 +0100

    Always follow links to version controlled files

--- .emacs.d/lisp/ux/prompts.el
+++ .emacs.d/lisp/ux/prompts.el
@@ -7,6 +7,10 @@
 
 (defun user/prompts-init ()
   "Initialize Emacs prompting."
+  (setq-default
+   ;; Always follow links to version controlled files.
+   vc-follow-symlinks t)
+
   ;; Use shorter y/n prompts instead of yes/no.
   (fset 'yes-or-no-p 'y-or-n-p)
 
