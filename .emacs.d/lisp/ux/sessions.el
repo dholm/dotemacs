87e32b0ba30a1e1bc68a100e3b18d95b61896f3d
David Holm <dholmster@gmail.com>
2014-01-27 16:42:50 +0100

    Remove buggy session package

--- .emacs.d/lisp/ux/sessions.el
+++ .emacs.d/lisp/ux/sessions.el
@@ -2,15 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun user/sessions-init ()
-  "Initialize Emacs session management."
-  (user/recentf-init)
-  (user/savehist-init)
-  (user/saveplace-init)
-
-  (require-package '(:name session :after (user/session-init))))
-
-
 (defun user/recentf-init ()
   "Initialize Emacs recent files history."
   (setq-default
@@ -37,32 +28,11 @@
    save-place t))
 
 
-(defun user/session-init ()
-  "Initialize session package."
-  (setq-default
-   session-save-file (path-join *user-cache-directory* "session")
-   desktop-globals-to-save
-   (append '((extended-command-history . 30)
-             (file-name-history        . 100)
-             (ido-last-directory-list  . 100)
-             (ido-work-directory-list  . 100)
-             (ido-work-file-list       . 100)
-             (grep-history             . 30)
-             (compile-history          . 30)
-             (minibuffer-history       . 50)
-             (query-replace-history    . 60)
-             (read-expression-history  . 60)
-             (regexp-history           . 60)
-             (regexp-search-ring       . 20)
-             (search-ring              . 20)
-             (comint-input-ring        . 50)
-             (shell-command-history    . 50)
-             desktop-missing-file-warning
-             tags-file-name
-             register-alist)))
-
-  (add-hook 'after-init-hook 'session-initialize))
-
+(defun user/sessions-init ()
+  "Initialize Emacs session management."
+  (user/recentf-init)
+  (user/savehist-init)
+  (user/saveplace-init))
 
 (user/sessions-init)
 
