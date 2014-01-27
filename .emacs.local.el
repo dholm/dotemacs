08571a4a6ed466b8956701488f989ed21f486bc6
David Holm <dholmster@gmail.com>
2014-01-27 09:17:43 +0100

    Include an EDE example in .emacs.local.el

--- .emacs.local.el
+++ .emacs.local.el
@@ -2,6 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 
+(after-load 'ede
+  ;; Generic EDE project example.
+  (when (file-exists-p "/path/to/project/root/file")
+    (ede-cpp-root-project "Project"
+                          :file "/path/to/project/root/file"
+                          :include-path '("libc" "libd")
+                          :system-include-path '()
+                          :spp-table '(("DEBUG" . ""))
+                          :compile-command "nice make -j")))
+
 
 (provide '.emacs.local)
 ;;; emacs.local.el ends here
