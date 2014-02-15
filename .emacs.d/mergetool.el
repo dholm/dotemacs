35f7c580cdb03e1337f763349d974d5a3261e61c
David Holm <dholmster@gmail.com>
2014-02-15 13:35:29 +0100

    Support for using Emacs as a mergetool

---
+++ .emacs.d/mergetool.el
@@ -0,0 +1,43 @@
+;;; mergetool.el --- Emacs as a mergetool
+;;; Commentary:
+;;; Usage:
+
+;; emacs --load mergetool.el
+
+;;; Code:
+
+(eval-and-compile
+  ;; Load Emacs init prologue.
+  (load (expand-file-name "prologue.el" user-emacs-directory)))
+
+(eval-when-compile
+  (require 'ediff))
+
+(require 'init-emacs)
+(require 'init-bindings)
+(require 'init-ux)
+(require 'init-vcs)
+
+
+(progn
+  (defun ediff-write-merge-buffer ()
+    (let ((file ediff-merge-store-file))
+      (set-buffer ediff-buffer-C)
+      (write-region (point-min) (point-max) file)
+      (message "Merge buffer saved in: %s" file)
+      (set-buffer-modified-p nil)
+      (sit-for 1)))
+  (setq ediff-quit-hook 'kill-emacs
+        ediff-quit-merge-hook 'ediff-write-merge-buffer)
+  (message "local: %s remote: %s base: %s merged: %s"
+           (getenv "LOCAL") (getenv "REMOTE")
+           (getenv "BASE") (getenv "MERGED"))
+  (ediff-merge-files-with-ancestor (getenv "LOCAL") (getenv "REMOTE")
+                                   (getenv "BASE") nil (getenv "MERGED")))
+
+
+;; Load Emacs init epilogue.
+(load (expand-file-name "epilogue.el" user-emacs-directory))
+
+
+;;; mergetool.el ends here
