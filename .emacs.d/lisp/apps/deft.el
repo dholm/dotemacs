2b635cd13e814b6682e2b1129b3161ea8ed11cc5
David Holm <dholmster@gmail.com>
2014-02-22 13:22:29 +0100

    Basic configuration for deft

--- .emacs.d/lisp/apps/deft.el
+++ .emacs.d/lisp/apps/deft.el
@@ -2,8 +2,31 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun user/deft-mode-hook ()
+  "Deft mode hook."
+  ;;; (Bindings) ;;;
+  (user/bind-key-local :basic :open-file-context 'deft-find-file))
+
+
 (defun user/deft-init ()
   "Initialize deft."
+  (setq-default
+   ;; The path to where notes will be stored.
+   deft-directory (path-join *user-documents-directory* "Notes")
+   ;; Set the default note format.
+   deft-extension "org"
+   deft-text-mode 'org-mode
+   ;; Enforce good file naming.
+   deft-use-filename-as-title t
+   ;; Auto-save idle interval in seconds.
+   deft-auto-save-interval 30.0)
+
+  (add-hook 'deft-mode-hook 'user/deft-mode-hook)
+
+  (after-load 'deft
+    ;; Run deft's setup after it has been loaded.
+    (deft-setup))
+
   ;;; (Bindings) ;;;
   (user/bind-key-global :apps :notes 'deft))
 
--- .emacs.d/lisp/init-constants.el
+++ .emacs.d/lisp/init-constants.el
@@ -19,6 +19,10 @@
   (getenv-or "XDG_CACHE_HOME"
              (path-join *user-home-directory* ".cache" "emacs"))
   "Path to user's local cache store.")
+(defconst *user-documents-directory*
+  (path-join *user-home-directory* "Documents")
+  "Path to user's documents directory.")
+
 (defconst *user-el-get-directory*
   (path-join user-emacs-directory "el-get")
   "Path to user's el-get store.")
