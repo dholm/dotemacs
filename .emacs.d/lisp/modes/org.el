b9d88b813862204afd6e88d6f5df58e7084829b8
David Holm <dholmster@gmail.com>
2014-02-22 14:10:51 +0100

    Improve org-mode configuration

--- .emacs.d/lisp/modes/org.el
+++ .emacs.d/lisp/modes/org.el
@@ -3,17 +3,57 @@
 ;;; Code:
 
 (defun user/org-mode-hook ()
-  "Org mode hook.")
+  "Org mode hook."
+  ;;; (Bindings) ;;;
+  (user/bind-key-local :code :context-promote 'org-shiftup)
+  (user/bind-key-local :code :context-demote 'org-shiftdown))
 
 
 (defun user/org-mode-init ()
   "Initialize Lua mode."
-  (require-package '(:name org-mode))
+  (setq-default
+   ;; Pressing return on a link follows it.
+   org-return-follows-link t
+   ;; Log time for TODO state changes.
+   org-log-done 'time
+   ;; Log time when rescheduling an entry.
+   org-log-reschedule 'time
+   org-log-redeadline 'time
+   ;; Log drawer state changes.
+   org-log-into-drawer t
+   ;; Allow single letter commands at beginning of headlines.
+   org-use-speed-commands t
+   ;; Fontify code blocks by default.
+   org-src-fontify-natively t
+   ;; Tab should operate according to local mode.
+   org-src-tab-acts-natively t)
 
-  (autoload 'org-add-link-type "org" "" t)
+  (when (not noninteractive)
+    (setq-default
+     ;; Remove clock line if time is zero.
+     org-clock-out-remove-zero-time-clocks t
+     ;; Stop clock when entry is marked as DONE.
+     org-clock-out-when-done t
+     ;; Show the amount of time spent on the current task today.
+     org-clock-modeline-total 'today
+     ;; Resume clock when reopening Emacs.
+     org-clock-persist t)
+
+    (after-load 'org-mode
+      ;; Initialize clock persistence.
+      (org-clock-persistence-insinuate))))
+
+  (when (display-graphic-p)
+    (setq-default
+     ;; Display inline images when starting up.
+     org-startup-with-inline-images t)
+
   (add-hook 'org-mode-hook 'user/org-mode-hook))
 
-(user/org-mode-init)
+(require-package '(:name org-mode :after (user/org-mode-init)))
+
+;; Fix for EIN if org hasn't been setup yet.
+(autoload 'org-add-link-type "org" "" t)
 
 
 (provide 'modes/org)
