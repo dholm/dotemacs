f25a72a3a9427c5ce5a79529876cc398e8fe24df
David Holm <dholmster@gmail.com>
2014-02-25 09:15:06 +0100

    Tweak Linux font

--- .emacs.d/lisp/ux/theme.el
+++ .emacs.d/lisp/ux/theme.el
@@ -17,7 +17,7 @@
           (set-face-attribute 'default nil :family "Consolas" :height 100 :weight 'normal))
          ((eq system-type 'gnu/linux)
           (set-face-attribute 'default nil :foundry "bitstream" :family "Meslo LG S DZ"
-                                         :height 100 :weight 'normal))))
+                                         :height 80 :weight 'normal))))
 
   ;; Enable blinking cursor
   (blink-cursor-mode)
