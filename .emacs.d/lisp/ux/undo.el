1830fb74531e2a778085a66dd1de46082c5b12c7
David Holm <dholmster@gmail.com>
2014-03-23 12:32:36 +0100

    Persistent undo history

--- .emacs.d/lisp/ux/undo.el
+++ .emacs.d/lisp/ux/undo.el
@@ -2,15 +2,36 @@
 ;;; Commentary:
 ;;; Code:
 
+(defconst *user-undo-tree-cache-directory*
+  (path-join *user-cache-directory* "undo-tree")
+  "Path to user's undo-tree cache store.")
+
+
 (defun user/undo-tree-init ()
   "Initialize undo-tree."
-  (require 'undo-tree)
+  ;; Ensure that cache store exists.
+  (make-directory *user-undo-tree-cache-directory* t)
+
+  (setq-default
+   ;; Set up undo history cache store.
+   undo-tree-history-directory-alist
+   `((".*" . ,*user-undo-tree-cache-directory*))
+   ;; Persistent undo history.
+   undo-tree-auto-save-history t)
 
   (global-undo-tree-mode t)
   (after-load 'diminish
     (diminish 'undo-tree-mode))
 
+  ;; Compress undo history.
+  (defadvice undo-tree-make-history-save-file-name
+    (after undo-tree activate)
+    (setq ad-return-value (concat ad-return-value ".gz")))
+
+  ;;; (Bindings) ;;;
   (global-set-key [remap undo] 'undo-tree-undo)
+  (global-set-key [remap redo] 'undo-tree-redo)
+  (user/bind-key-global :basic :undo 'undo-tree-undo)
   (user/bind-key-global :basic :redo 'undo-tree-redo)
   (user/bind-key-global :util :undo-tree 'undo-tree-visualize))
 
