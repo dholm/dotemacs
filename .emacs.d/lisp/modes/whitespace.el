92e9f23333f2ca27f2f76cbf83fae97845c28eea
David Holm <dholmster@gmail.com>
2014-04-27 12:36:32 +0200

    Fancier whitespace-mode

--- .emacs.d/lisp/modes/whitespace.el
+++ .emacs.d/lisp/modes/whitespace.el
@@ -5,12 +5,39 @@
 (defun user/whitespace-init ()
   "Initialize whitespace mode."
   (setq-default
-   ;; Always show trailing whitespace
+   ;; Always show trailing whitespace.
    show-trailing-whitespace t
-   whitespace-style '(trailing lines space-before-tab indentation space-after-tab)
-   whitespace-line-column 400)
+   ;; Maximum allowed line length.
+   whitespace-line-column 80
+   ;; Kinds of whitespace to visualize.
+   whitespace-style
+   '(;; Visualize using faces.
+     face
+     ;; Mark any tabs.
+     tab-mark
+     ;; Empty lines at beginning or end of buffer.
+     empty
+     ;; Trailing whitespace.
+     trailing
+     ;; Lines that extend beyond `whitespace-line-column.'
+     lines
+     ;; Wrong kind of indentation (tab when spaces and vice versa.)
+     indentation
+     ;; Mixture of space and tab on the same line.
+     space-before-tab space-after-tab))
 
-  ;; Enable whitespace mode globally
+  (when (eq default-terminal-coding-system 'utf-8)
+    (setq-default
+     ;; Special characters for displaying whitespace.
+     whitespace-display-mappings
+     '(;; 32 SPACE, 183 MIDDLE DOT 「·」, 46 FULL STOP 「.」
+       (space-mark 32 [183] [46])
+       ;; 10 LINE FEED, 9166 RETURN SYMBOL 「⏎」, 36 DOLLAR SIGN 「$」
+       (newline-mark 10 [9166 10] [36 10])
+       ;; 9 TAB, 9655 WHITE RIGHT-POINTING TRIANGLE 「▷」, 92 9 CHARACTER TABULATION 「\t」
+       (tab-mark 9 [9655 9] [92 9]))))
+
+  ;; Enable whitespace mode globally.
   (global-whitespace-mode t)
   (after-load 'diminish
     (diminish 'global-whitespace-mode)))
