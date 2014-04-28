49527d8ae00757fb73ea9b5db4fa25dd6fe6d168
David Holm <dholmster@gmail.com>
2014-04-28 10:19:16 +0200

    Use doc-mode-nschum to insert code documentation

--- .emacs.d/lisp/modes/doxygen.el
+++ .emacs.d/lisp/modes/doxygen.el
@@ -22,44 +22,8 @@
     (doxymacs-font-lock))
 
   ;;; (Bindings) ;;;
-  (user/bind-key-local :code :document 'doxygen-insert-dwim))
-
-
-(defun doxygen-insert-fn (point)
-  "Get insertion function at POINT."
-  (let ((ctxt (semantic-analyze-current-context))
-        (pfx nil)
-        (tag nil)
-        (class nil))
-    (when ctxt
-      (setq pfx (reverse (oref ctxt :prefix)))
-      (while (and (not tag) pfx)
-        (when (semantic-tag-p (car pfx))
-          (setq tag (car pfx)))
-        (setq pfx (cdr pfx))))
-    (if tag
-        (let ((is-member (or (eq (semantic--tag-get-property tag 'reparse-symbol)
-                                 'enumsubparts)))
-              (is-function (semantic-tag-of-class-p tag 'function)))
-          (cond (is-member (doxymacs-insert-member-comment))
-                (is-function (progn
-                               (beginning-of-line)
-                               (newline-and-indent)
-                               (forward-line -1)
-                               (indent-according-to-mode)
-                               (doxymacs-insert-function-comment)))
-                (t nil)))
-      (let ((is-first-line (eq (count-lines 1 point) 0)))
-        (cond ((region-active-p) (doxymacs-insert-grouping-comments
-                                  (region-beginning) (region-end)))
-              (is-first-line (doxymacs-insert-file-comment)))))))
-
-
-(defun doxygen-insert-dwim ()
-  "Blah."
-  (interactive)
-  (save-excursion
-    (doxygen-insert-fn (point))))
+  (with-feature 'doc-mode
+    (user/bind-key-local :code :document 'doc-mode-fix-tag-doc)))
 
 
 (defun doxygen-mode (&optional arg)
@@ -71,7 +35,9 @@
           (> (prefix-numeric-value arg) 0)))
   (when doxygen-mode
     (with-feature 'doxymacs
-      (doxymacs-mode))
+      (doxymacs-mode t))
+    (with-feature 'doc-mode
+      (doc-mode t))
     (run-hooks 'doxygen-mode-hook)))
 
 
@@ -79,8 +45,10 @@
   "Initialize Doxygen mode."
   (add-hook 'doxygen-mode-hook 'user/doxygen-mode-hook)
 
+  ;;; (Packages) ;;;
   (when (pkg-config-has-p "libxml-2.0")
-    (require-package '(:name doxymacs))))
+    (require-package '(:name doxymacs)))
+  (require-package '(:name doc-mode-nschum)))
 
 (with-executable 'doxygen
   (user/doxygen-mode-init))
