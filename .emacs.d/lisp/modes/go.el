3190d49d77de7377e602102eb882c63862040528
David Holm <david.holm@ericsson.com>
2014-07-03 10:15:40 +0200

    modes: Go mode improvements
    Support for eldoc and projectile as well as some other new packages.

--- .emacs.d/lisp/modes/go.el
+++ .emacs.d/lisp/modes/go.el
@@ -10,6 +10,12 @@
                ;; Fix formatting
                (gofmt-before-save)))
 
+  (when (feature-p 'go-eldoc)
+    (go-eldoc-setup))
+
+  (when (feature-p 'go-oracle)
+    (go-oracle-mode t))
+
   ;;; (Bindings) ;;;
   (user/bind-key-local :doc :describe 'godef-describe)
   (user/bind-key-local :doc :reference 'godoc)
@@ -27,7 +33,10 @@
 (with-executable 'go
   (require-package '(:name go-mode :after (user/go-mode-init)))
   (require-package '(:name go-autocomplete))
-  (require-package '(:name go-test)))
+  (require-package '(:name go-eldoc))
+  (require-package '(:name go-test))
+  (require-package '(:name go-projectile))
+  (require-package '(:name go-oracle)))
 
 
 (provide 'modes/go)
