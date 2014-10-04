902ba1a4f7351a44a26b09a741e302ddf6898ba9
David Holm <dholmster@gmail.com>
2014-10-04 10:40:51 +0200

    modes/perl: Replace PDE with Sepia

--- .emacs.d/lisp/modes/perl.el
+++ .emacs.d/lisp/modes/perl.el
@@ -5,37 +5,27 @@
 (defun user/perl-mode-hook ()
   "Perl mode hook."
   (auto-complete-mode t)
-  (add-ac-sources 'ac-source-perl-completion)
-
-  ;; Initialize PDE
-  (user/pde-perl-mode-hook))
+  (add-ac-sources 'ac-source-perl-completion))
 
 
-(defun user/pde-perl-mode-hook ()
-  "PDE Perl mode hook."
-  (with-feature 'pde-load
-    (setq-default
-     compilation-buffer-name-function 'pde-compilation-buffer-name)
-
-    ;; chmod when saving
-    (when (and buffer-file-name
-               (not (string-match "\\.\\(pm\\|pod\\)$" (buffer-file-name))))
-      (add-hook 'after-save-hook 'executable-chmod nil t))
-    (set (make-local-variable 'compile-dwim-check-tools) nil)
-
-    ;;; (Bindings) ;;;
-    (user/bind-key-local :code :compile 'compile-dwim-compile)
-    (user/bind-key-local :code :run 'compile-dwim-run)))
+(defun user/sepia-mode-hook ()
+  "Sepia mode hook."
+  (user/perl-mode-hook))
 
 
 (defun user/perl-mode-init ()
   "Initialize Perl mode."
+  (require-package '(:name cperl-mode))
+  (require-package '(:name sepia))
   (require-package '(:name perl-completion
                            :type emacswiki
                            :website "https://raw.github.com/emacsmirror/emacswiki.org/master/perl-completion.el"))
-  (require-package '(:name pde))
 
-  (add-hook 'perl-mode-hook 'user/perl-mode-hook))
+  ;; Use Sepia as the default perl mode.
+  (defalias 'perl-mode 'sepia-mode)
+
+  (add-hook 'perl-mode-hook 'user/perl-mode-hook)
+  (add-hook 'sepia-mode-hook 'user/sepia-mode-hook))
 
 (with-executable 'perl
   (user/perl-mode-init))
