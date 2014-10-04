ddd439f084a7277aa8106e40cfd692d646dd55e7
David Holm <dholmster@gmail.com>
2014-10-04 15:01:25 +0200

    ux/buffers: Helpers for closing buffers except global ones

--- .emacs.d/lisp/init-utilities.el
+++ .emacs.d/lisp/init-utilities.el
@@ -5,12 +5,5 @@
 (load-all-files-from-dir (path-join *user-emacs-lisp-directory* "utilities"))
 
 
-;;; (Functions) ;;;
-(defun user/close-all-buffers ()
-  "Close all open buffers."
-  (interactive)
-  (mapc 'kill-buffer (buffer-list)))
-
-
 (provide 'init-utilities)
 ;;; init-utilities.el ends here
--- .emacs.d/lisp/ux/buffers.el
+++ .emacs.d/lisp/ux/buffers.el
@@ -2,6 +2,32 @@
 ;;; Commentary:
 ;;; Code:
 
+(defun user/kill-matching-buffers (regexp &optional exclude-p)
+  "Kill buffers whose name match the specified REGEXP but not EXCLUDE-P."
+  (interactive "sKill buffers matching this regular expression: \nP")
+  (dolist (buffer (buffer-list))
+    (let ((name (buffer-name buffer)))
+      (when (and name (not (string-equal name ""))
+                 (/= (aref name 0) ?\s)
+                 (string-match regexp name))
+        (unless (and exclude-p (funcall exclude-p buffer))
+          (kill-buffer buffer))))))
+
+
+(defun user/kill-all-buffers ()
+  "Close all open buffers."
+  (interactive)
+  (user/kill-matching-buffers "^[^\*].+[^\*]$")
+  (switch-to-buffer "*scratch*"))
+
+
+(defun user/kill-other-buffers ()
+  "Close all open buffers except current one."
+  (interactive)
+  (user/kill-matching-buffers "^[^\*].+[^\*]$"
+                              (lambda (x) (eq (current-buffer) x))))
+
+
 (defun user/buffers-init ()
   "Initialize Emacs buffers."
   (setq-default
