51cbb7e9cfe4034ae3acf5dd5f31aa02ca5f6a64
David Holm <dholmster@gmail.com>
2014-10-10 21:15:46 +0200

    modes: Scheme mode support

---
+++ .emacs.d/lisp/modes/scheme.el
@@ -0,0 +1,55 @@
+;;; scheme.el --- Set up Scheme mode
+;;; Commentary:
+;;; Code:
+
+(defun user/scheme-mode-hook ()
+  "Scheme mode hook."
+  (with-feature 'paredit
+    (enable-paredit-mode)))
+
+
+(defun user/geiser-mode-hook ()
+  "Geiser mode hook."
+  (with-feature 'ac-geiser
+    (ac-geiser-setup)))
+
+
+(defun user/geiser-repl-mode-hook ()
+  "Geiser REPL mode hook."
+  (with-feature 'ac-geiser
+    (ac-geiser-setup)))
+
+
+(defun user/geiser-init ()
+  "Initialize Geiser."
+  (when (feature-p 'ac-geiser)
+    (after-load 'geiser
+      (add-ac-modes 'geiser-repl-mode)))
+
+  ;;; (Hooks) ;;;
+  (add-hook 'geiser-mode-hook 'user/geiser-mode-hook)
+  (add-hook 'geiser-repl-mode-hook 'user/geiser-repl-mode-hook))
+
+
+(defun user/quack-init ()
+  "Initialize Quack."
+  (setq-default
+   ;; Use Emacs-style fontification.
+   quack-fontify-style 'emacs))
+
+
+(defun user/scheme-mode-init ()
+  "Initialize Scheme mode."
+  ;;; (Hooks) ;;;
+  (add-hook 'scheme-mode-hook 'user/scheme-mode-hook)
+
+  ;;; (Packages) ;;;
+  (require-package '(:name geiser :after (user/geiser-init)))
+  (require-package '(:name quack :after (user/quack-init)))
+  (require-package '(:name ac-geiser)))
+
+(user/scheme-mode-init)
+
+
+(provide 'modes/scheme)
+;;; scheme.el ends here
