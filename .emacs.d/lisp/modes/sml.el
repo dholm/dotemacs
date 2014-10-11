16039caecdc24e3038dd04661040b8d03c67ee17
David Holm <dholmster@gmail.com>
2014-10-11 12:23:56 +0200

    modes: SML mode support

---
+++ .emacs.d/lisp/modes/sml.el
@@ -0,0 +1,23 @@
+;;; sml.el --- Set up SML mode
+;;; Commentary:
+;;; Code:
+
+(defun user/sml-mode-hook ()
+  "SML mode hook.")
+
+
+(defun user/sml-mode-init ()
+  "Initialize SML mode."
+  (add-auto-mode 'sml-mode "\\.[Mm][Ll]$")
+
+  ;;; (Hooks) ;;;
+  (add-hook 'sml-mode-hook 'user/sml-mode-hook)
+
+  ;;; (Packages) ;;;
+  (require-package '(:name sml-mode :after (user/sml-mode-init))))
+
+(user/sml-mode-init)
+
+
+(provide 'modes/sml)
+;;; sml.el ends here
