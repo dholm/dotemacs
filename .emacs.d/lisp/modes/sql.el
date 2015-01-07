603cee9329b1f68580932c431380674c6133c73b
David Holm <dholmster@gmail.com>
2015-01-07 12:37:53 +0100

    modes: Very basic SQL mode support

---
+++ .emacs.d/lisp/modes/sql.el
@@ -0,0 +1,30 @@
+;;; sql.el --- Initializes SQL mode
+;;; Commentary:
+;;; Code:
+
+(defun user/sql-mode-hook ()
+  "SQL mode hook.")
+
+
+(defun user/sqli-buffer ()
+  "Switch to the corresponding sqli buffer."
+  (interactive)
+  (with-feature 'sql
+    (if sql-buffer
+        (progn
+          (pop-to-buffer sql-buffer)
+          (goto-char (point-max)))
+      (sql-set-sqli-buffer)
+      (when sql-buffer
+        (user/sqli-buffer)))))
+
+
+(defun user/sql-init ()
+  "Initialize SQL mode."
+  (add-hook 'sql-mode-hook 'user/sql-mode-hook))
+
+(user/sql-init)
+
+
+(provide 'modes/sql)
+;;; sql.el ends here
