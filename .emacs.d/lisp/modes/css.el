2d3fab76dd79d3793eca0fa9d272e5ce52896ea2
David Holm <dholmster@gmail.com>
2015-01-15 14:13:22 +0100

    modes/css: Use helm-css-scss for CSS navigation

--- .emacs.d/lisp/modes/css.el
+++ .emacs.d/lisp/modes/css.el
@@ -14,19 +14,26 @@
 
   (showcss-mode t)
   (skewer-css-mode t)
-  (rainbow-mode t))
+  (rainbow-mode t)
+
+  ;;; (Bindings) ;;;
+  (user/bind-key-local :nav :functions/toc 'helm-css-scss)
+  (user/bind-key-local :nav :go-back 'helm-css-scss-back-to-last-point))
 
 
 (defun user/css-mode-init ()
   "Initialize CSS mode."
+  (add-hook 'css-mode-hook 'user/css-mode-hook)
+
+  ;;; (Packages) ;;;
   (require-package '(:name showcss-mode))
   (require-package '(:name rainbow-mode
                            :type http
                            :url "http://elpa.gnu.org/packages/rainbow-mode-0.9.el"
                            :build '(("mv" "rainbow-mode-0.9.el" "rainbow-mode.el"))
                            :compile "rainbow-mode.el"))
-
-  (add-hook 'css-mode-hook 'user/css-mode-hook))
+  (when (feature-p 'helm)
+    (require-package '(:name helm-css-scss))))
 
 (user/css-mode-init)
 
