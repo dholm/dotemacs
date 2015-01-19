e8c51e7406de241b714bdf64560bb18e03f58623
David Holm <dholmster@gmail.com>
2015-01-19 14:41:00 +0100

    utilities/tls: Make GnuTLS more lenient

--- .emacs.d/lisp/utilities/tls.el
+++ .emacs.d/lisp/utilities/tls.el
@@ -5,10 +5,11 @@
 (defun user/tls-init ()
   "Initialize Emacs TLS communications."
   (setq-default
-   ;; Default to OpenSSL, hten fall back to GnuTLS.
+   ;; Default GnuTLS arguments.
+   starttls-extra-arguments '("--priority" "secure256" "--disable-extensions")
+   ;; Default to OpenSSL, then fall back to GnuTLS.
    tls-program '("openssl s_client -connect %h:%p -no_ssl2 -ign_eof"
-                 "gnutls-cli --priority secure256 -p %p %h"
-                 "gnutls-cli --priority secure256 -p %p %h")))
+                 "gnutls-cli --priority secure256 --disable-extensions -p %p %h")))
 
 (user/tls-init)
 
