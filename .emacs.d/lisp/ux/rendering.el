54e52a98868c22f3ba6363e4174b7b54a2c44564
David Holm <dholmster@gmail.com>
2015-01-21 19:02:32 +0100

    ux/rendering: Do not override default idle update time

--- .emacs.d/lisp/ux/rendering.el
+++ .emacs.d/lisp/ux/rendering.el
@@ -5,8 +5,6 @@
 (defun user/rendering-init ()
   "Setup Emacs user interface rendering."
   (setq-default
-   ;; Update Emacs display every ten seconds when idle.
-   idle-update-delay 10
    ;; Redraw the entire screen before checking for pending input events.
    ;; This will improve performance in general but might degrade performance of
    ;; key repeat.
