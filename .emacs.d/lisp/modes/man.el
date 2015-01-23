c513e4fd48d338fa0983587b80973e7c58fac53e
David Holm <dholmster@gmail.com>
2015-01-23 09:27:39 +0100

    modes/man: Configure and switch to WoMan

--- .emacs.d/lisp/modes/man.el
+++ .emacs.d/lisp/modes/man.el
@@ -2,17 +2,41 @@
 ;;; Commentary:
 ;;; Code:
 
+(defconst *user-man-cache-directory*
+ (path-join *user-cache-directory* "man")
+ "Path to user's man cache store.")
+
+
+(defun user/woman-init ()
+  "Initialize Emacs WoMan."
+  (setq-default
+   ;; WoMan cache store.
+   woman-cache-file-name (path-join *user-man-cache-directory* "wmcache.el")
+   ;; Use highest cache level.
+   woman-cache-level 3
+   ;; Have WoMan fill the entire frame.
+   woman-fill-frame t)
+
+  ;;; (Bindings) ;;;
+  (user/bind-key-global :doc :manual 'woman))
+
+
 (defun user/man-mode-init ()
   "Initialize Emacs man-mode."
   (setq-default
    ;; Make man-mode wrap lines at half the width of Emacs.
-   Man-width (/ (window-total-width (frame-root-window)) 2))
+   Man-width (/ (window-total-width (frame-root-window)) 2)))
 
-  ;;; (Bindings) ;;;
-  (user/bind-key-global :doc :manual 'man))
+
+(defun user/man-init ()
+  "Initialize Emacs man support."
+  (user/man-mode-init)
+  (user/woman-init)
+
+  (make-directory *user-man-cache-directory* t))
 
 (with-executable 'man
-  (user/man-mode-init))
+  (user/man-init))
 
 
 (provide 'modes/man)
