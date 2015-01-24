b6a719a742ab55e16c5eead100e8fbd0ece78ba6
David Holm <dholmster@gmail.com>
2015-01-24 21:32:05 +0100

    utilities: Basic Hide Show configuration

---
+++ .emacs.d/lisp/utilities/hideshow.el
@@ -0,0 +1,30 @@
+;;; hideshow.el --- Configure hide show
+;;; Commentary:
+;;; Code:
+
+(defun user/hs-minor-mode-hook ()
+  "Minor mode hook for Hide Show."
+  ;;; (Bindings) ;;;
+  (local-set-key (kbd "C-c SPC") 'user/hs-toggle-level)
+  (local-set-key (kbd "C-c <right>") 'hs-show-block)
+  (local-set-key (kbd "C-c <left>")  'hs-hide-block)
+  (local-set-key (kbd "C-c <up>")    'hs-hide-all)
+  (local-set-key (kbd "C-c <down>")  'hs-show-all))
+
+
+(defun user/hs-toggle-level ()
+  "Toggle hide/show for level at point."
+  (interactive)
+  (hs-show-block)
+  (hs-hide-level 1))
+
+
+(defun user/hideshow-init ()
+  "Initialize Hide Show mode."
+  ;;; (Hooks) ;;;
+  (add-hook 'hs-minor-mode-hook 'user/hs-minor-mode-hook))
+
+(user/hideshow-init)
+
+(provide 'utilities/hideshow)
+;;; hideshow.el ends here
