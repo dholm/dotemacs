f783587ce661bbb03d8ea1fdd86b009c19827c06
David Holm <dholmster@gmail.com>
2015-02-02 18:01:04 +0100

    ux/prompts: Don't use GUI when prompting

--- .emacs.d/lisp/ux/prompts.el
+++ .emacs.d/lisp/ux/prompts.el
@@ -11,6 +11,11 @@
   ;; Use shorter y/n prompts instead of yes/no.
   (fset 'yes-or-no-p 'y-or-n-p)
 
+  (when (display-graphic-p)
+    (setq-default
+     ;; Don't use graphical dialog boxes when prompting.
+     use-dialog-box nil))
+
   (defadvice save-buffers-kill-emacs (around no-query-kill-emacs activate)
     "Prevent \"Active processes exist\" query when you quit Emacs."
     (with-feature 'cl-lib
