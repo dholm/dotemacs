8a5e3e3e1a7c0b9feb572e4f96f44a5c5dd770c7
David Holm <dholmster@gmail.com>
2015-02-07 14:58:38 +0100

    utilities/flycheck: Acquire configuration from EDE

--- .emacs.d/lisp/utilities/flycheck.el
+++ .emacs.d/lisp/utilities/flycheck.el
@@ -9,6 +9,40 @@
   (user/bind-key-local :nav :next 'flycheck-next-error))
 
 
+(defun user/ede-flycheck-setup ()
+  "Configure `flycheck' using `ede'."
+  (when (and ede-object
+             (slot-exists-p ede-object 'compilation)
+             (oref ede-object compilation))
+    (let* ((comp (oref ede-object compilation))
+           (cmd (get-command-line comp)))
+      (setq
+       flycheck-clang-includes (get-includes comp)
+       flycheck-clang-definitions (get-defines comp)
+       flycheck-clang-include-path (get-include-path comp t))
+
+      (when (string-match " -std=\\([^ ]+\\)" cmd)
+        (setq
+         flycheck-clang-language-standard (match-string 1 cmd)
+         flycheck-gcc-language-standard (match-string 1 cmd)))
+      (when (string-match " -stdlib=\\([^ ]+\\)" cmd)
+        (setq flycheck-clang-standard-library (match-string 1 cmd)))
+      (when (string-match " -fms-extensions " cmd)
+        (setq flycheck-clang-ms-extensions t))
+      (when (string-match " -fno-exceptions " cmd)
+        (setq
+         flycheck-clang-no-exceptions t
+         flycheck-gcc-no-exceptions t))
+      (when (string-match " -fopenmp" cmd)
+        (setq flycheck-gcc-openmp t))
+      (when (string-match " -fno-rtti " cmd)
+        (setq
+         flycheck-clang-no-rtti t
+         flycheck-gcc-no-rtti t))
+      (when (string-match " -fblocks " cmd)
+        (setq flycheck-clang-blocks t)))))
+
+
 (defun user/flycheck-color-mode-line-init ()
   "Initialize flycheck color mode."
   (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
@@ -27,7 +61,10 @@
     ;; Use popwin for Flycheck error list.
     (push '(flycheck-error-list-mode :stick t) popwin:special-display-config))
 
-  (add-hook 'flycheck-mode-hook 'user/flycheck-mode-hook))
+  ;;; (Hooks) ;;;
+  (add-hook 'flycheck-mode-hook 'user/flycheck-mode-hook)
+  (add-hook 'ede-minor-mode-hook 'user/ede-flycheck-setup)
+  (add-hook 'ede-compdb-project-rescan-hook 'user/ede-flycheck-setup))
 
 (require-package '(:name flycheck :after (user/flycheck-init)))
 
