27ef1a75ce7794f0f3284efc44c1b21b217f5439
David Holm <dholmster@gmail.com>
2015-05-20 17:54:35 +0200

    apps: eudc: Include metadata in search results

--- .emacs.d/lisp/apps/eudc.el
+++ .emacs.d/lisp/apps/eudc.el
@@ -5,14 +5,19 @@
 (defun user/eudc-expand-inline ()
   "Expand entry at point using EUDC."
   (interactive)
-  (if (eq eudc-protocol 'ldap)
-      (progn (move-end-of-line 1)
-             (insert "*")
-             (unless (condition-case nil
-                         (eudc-expand-inline)
-                       (error nil))
-               (backward-delete-char-untabify 1)))
-    (eudc-expand-inline)))
+  (cond
+   ((eq eudc-protocol 'ldap)
+    (progn
+      (move-end-of-line 1)
+      (insert "*")
+      (unless (condition-case nil
+                  (eudc-expand-inline)
+                (error nil))
+        (backward-delete-char-untabify 1))))
+   (t (eudc-expand-inline)))
+  ;; Remove metadata from email after expansion.
+  (when (re-search-backward " {.*}" nil t)
+    (replace-match "")))
 
 
 (defun user/eudc-init ()
@@ -33,7 +38,7 @@
 
     (eudc-protocol-set
      'eudc-inline-expansion-format
-     '("%s <%s>" displayName email)
+     '("%s <%s> {%s %s}" displayName email company department)
      'ldap)
     (eudc-protocol-set
      'eudc-inline-query-format
@@ -43,7 +48,7 @@
   (after-load 'bbdb
     (eudc-protocol-set
      'eudc-inline-expansion-format
-     '("%s %s <%s>" firstname lastname net)
+     '("%s %s <%s> {%s}" firstname lastname net company)
      'bbdb)
     (eudc-protocol-set
      'eudc-inline-query-format
