280f2a205a82ecafe7ef198fb136276c3e830a89
David Holm <dholmster@gmail.com>
2015-05-22 17:41:01 +0200

    modes: markdown: Support for live previews

--- .emacs.d/lisp/modes/markdown.el
+++ .emacs.d/lisp/modes/markdown.el
@@ -11,6 +11,8 @@
   (add-hook 'markdown-mode-hook 'user/markdown-mode-hook))
 
 (require-package '(:name markdown-mode :after (user/markdown-mode-init)))
+(with-executable 'npm
+  (require-package '(:name livedown)))
 
 
 (provide 'modes/markdown)
