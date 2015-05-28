bc940c801b13de724c726cc0f28acc828cb24427
David Holm <dholmster@gmail.com>
2015-05-28 16:54:36 +0200

    ux: rendering: Reduce font lock level in large buffers

--- .emacs.d/lisp/ux/rendering.el
+++ .emacs.d/lisp/ux/rendering.el
@@ -2,13 +2,33 @@
 ;;; Commentary:
 ;;; Code:
 
+(defconst *user-fast-font-lock-buffer-limit* (* 128 1024)
+  "Maximum buffer size for maximum font lock decorations.")
+
+
+(defun user/rendering-find-file-hook ()
+  "Rendering hook for `find-file'."
+  (when (> (buffer-size) *user-fast-font-lock-buffer-limit*)
+    (setq
+     ;; Reduce the level of fontification.
+     font-lock-maximum-decoration nil)
+
+    (font-lock-refresh-defaults)))
+
+
 (defun user/rendering-init ()
   "Setup Emacs user interface rendering."
+  (after-load 'font-lock
+    ;; Allow reduction of decoration level in large buffers.
+    (make-variable-buffer-local 'font-lock-maximum-decoration))
+
   (setq-default
    ;; Redraw the entire screen before checking for pending input events.
    ;; This will improve performance in general but might degrade performance of
    ;; key repeat.
    redisplay-dont-pause t
+   ;; Use the maximum amount of decorations by default.
+   font-lock-maximum-decoration t
    ;; Always use JIT mode for font-lock.
    font-lock-support-mode 'jit-lock-mode
    ;; Set up font-lock JIT mode to do background parsing.
@@ -19,6 +39,9 @@
    jit-lock-chunk-size 500
    ;; Do not reorder text during rendering.
    bidi-display-reordering nil)
+
+  ;;; (Hooks) ;;;
+  (add-hook 'find-file-hook 'user/rendering-find-file-hook)
 
   ;;; (Bindings) ;;;
   (user/bind-key-global :emacs :redraw 'redraw-display))
