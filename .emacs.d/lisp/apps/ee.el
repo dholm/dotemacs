d8857bf0afedb7553c89eead45266635a6f76c93
David Holm <david.holm@ericsson.com>
2017-02-20 13:36:52 +0100

    apps: ee: Switch to quelpa

--- .emacs.d/lisp/apps/ee.el
+++ .emacs.d/lisp/apps/ee.el
@@ -6,19 +6,19 @@
   (path-join *user-data-directory* "ee")
   "Path to user's ee data store.")
 
-
-(defun user--ee-config ()
-  "Initialize ee."
-  (after-load 'ee
-    (validate-setq
-     ;; Database storage location.
-     ee-data-directory *user-ee-data-directory*
-     ee-view-data-directory *user-ee-data-directory*))
+(use-package ee
+  :defer t
+  :quelpa (ee
+           :fetcher git
+           :url "http://git.savannah.gnu.org/r/ee.git")
+  :config
+  (validate-setq
+   ;; Database storage location.
+   ee-data-directory *user-ee-data-directory*
+   ee-view-data-directory *user-ee-data-directory*)
 
   ;;; (Bindings) ;;;
   (user/bind-key-global :apps :information-db 'ee))
-
-(require-package '(:name ee :after (user--ee-config)))
 
 
 (provide 'apps/ee)
