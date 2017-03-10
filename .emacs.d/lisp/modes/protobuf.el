56455ee36b48600022a74de9359728e0bf2a2b0d
David Holm <david.holm@ericsson.com>
2017-03-10 15:54:14 +0100

    modes: Protobuf mode

---
+++ .emacs.d/lisp/modes/protobuf.el
@@ -0,0 +1,11 @@
+;;; protobuf.el --- Protobuf mode support -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package protobuf-mode
+  :defer
+  :mode (("proto\\'" . protobuf-mode)))
+
+
+(provide 'modes/protobuf)
+;;; protobuf.el ends here
