8a6570828dbe11c85bfea26849a02cc549d2ec3b
David Holm <david.holm@ericsson.com>
2017-07-26 09:00:43 +0200

    ux: symbols: Use all-the-icons on graphical displays

--- .emacs.d/lisp/ux/symbols.el
+++ .emacs.d/lisp/ux/symbols.el
@@ -2,19 +2,17 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun user--symbols-config ()
-  "Initialize Emacs symbol handling."
-  ;;; (Packages) ;;;
-  (use-package page-break-lines
-    :diminish page-break-lines-mode
-    :init
-    (global-page-break-lines-mode t))
+(use-package page-break-lines
+  :diminish page-break-lines-mode
+  :init
+  (global-page-break-lines-mode t))
 
-  (with-feature 'ux/coding
-    (when (eq default-terminal-coding-system 'utf-8)
-      (use-package pretty-mode-plus))))
+(with-eval-after-load 'ux/coding
+  (when (eq default-terminal-coding-system 'utf-8)
+    (use-package pretty-mode-plus)))
 
-(user--symbols-config)
+(use-package all-the-icons
+  :if window-system)
 
 
 (provide 'ux/symbols)
