df1ff6334312f32a080ad7b4a013e297b9917484
David Holm <david.holm@ericsson.com>
2017-09-06 14:57:28 +0200

    modes: Read mode support for epub

---
+++ .emacs.d/lisp/modes/epub.el
@@ -0,0 +1,11 @@
+;;; epub --- initializes EPUB reader mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package nov
+  :defer
+  :mode ("\\.epub\\'" . nov-mode))
+
+
+(provide 'modes/epub)
+;;; epub.el ends here
