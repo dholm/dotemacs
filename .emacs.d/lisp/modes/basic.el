d752e0521fa7239402913adb4209aa37fb9da5ea
David Holm <david.holm@ericsson.com>
2017-10-03 11:19:35 +0200

    modes: BASIC mode support

---
+++ .emacs.d/lisp/modes/basic.el
@@ -0,0 +1,10 @@
+;;; basic --- Initializes BASIC mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package basic-mode
+  :mode "\.bas$")
+
+
+(provide 'modes/basic)
+;;; basic.el ends here
