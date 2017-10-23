c7ae00f029f3a204dc1c432c7d70aa4980ee7338
David Holm <david.holm@ericsson.com>
2017-10-23 11:34:26 +0200

    modes: docker: Extract docker utils
    Extract docker utilities from modes to utilities.

--- .emacs.d/lisp/modes/docker.el
+++ .emacs.d/lisp/modes/docker.el
@@ -2,14 +2,6 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package docker
-  :if (executable-find "docker")
-  :config
-  (use-package docker-tramp)
-
-  ;; Enabled docker support globally.
-  (docker-global-mode t))
-
 (use-package dockerfile-mode)
 (use-package docker-compose-mode)
 
---
+++ .emacs.d/lisp/utilities/docker.el
@@ -0,0 +1,15 @@
+;;; docker.el --- Configure Emacs docker support -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package docker
+  :if (executable-find "docker")
+  :config
+  (use-package docker-tramp)
+
+  ;; Enabled docker support globally.
+  (docker-global-mode t))
+
+
+(provide 'utilities/docker)
+;;; docker.el ends here
