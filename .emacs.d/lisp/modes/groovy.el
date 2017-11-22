c62d00f6dae12233c2c8e6528db580c4e2d9b09a
David Holm <david.holm@ericsson.com>
2017-11-22 13:33:13 +0100

    modes: groovy: Groovy mode for Jenkinsfile

---
+++ .emacs.d/lisp/modes/groovy.el
@@ -0,0 +1,10 @@
+;;; groovy --- Initializes Groovy mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package groovy-mode
+  :mode "Jenkinsfile")
+
+
+(provide 'modes/groovy)
+;;; groovy.el ends here
