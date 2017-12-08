026f14c049871b2e2c1727c6ea09bed6b3151f19
David Holm <david.holm@ericsson.com>
2017-12-08 16:12:49 +0100

    utilities: sauron: Start on Emacs startup

--- .emacs.d/lisp/utilities/sauron.el
+++ .emacs.d/lisp/utilities/sauron.el
@@ -6,10 +6,15 @@
   :defer
   :init
   (user/bind-key-global :util :notifications 'sauron-toggle-hide-show)
+
+  (sauron-start-hidden)
   :config
   (validate-setq
    ;; Display sauron in current frame.
-   sauron-separate-frame nil))
+   sauron-separate-frame nil)
+
+  (with-eval-after-load 'alert
+    (add-hook 'sauron-event-added-functions 'sauron-alert-el-adapter)))
 
 
 (provide 'utilities/sauron)
