09223681ad6d977b90a18c0acfe508b808676bf0
David Holm <david.holm@ericsson.com>
2018-02-16 16:16:35 +0100

    utilities: smtpmail: Use asynchronous SMTP

--- .emacs.d/lisp/utilities/smtpmail.el
+++ .emacs.d/lisp/utilities/smtpmail.el
@@ -2,6 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
+(defconst *user-smtpmail-data-directory*
+  (path-join *user-data-directory* "smtpmail")
+  "Path to user's smtpmail data store.")
+
 (defun user/smtpmail-set-gmail-user (fullname username)
   "Configure smtpmail to use \"FULLNAME\" <USERNAME@gmail.com>."
   (let ((email-address (concat username "@gmail.com")))
@@ -23,20 +27,36 @@
 
 (use-package smtpmail
   :defer
+  :init
+  ;; Create directory for send queue.
+  (let ((smtpmail-queue-dir
+         (path-join *user-smtpmail-data-directory* "send-queue")))
+    (make-directory smtpmail-queue-dir t)
+    (set-file-modes smtpmail-queue-dir #o0700))
   :config
   (validate-setq
+   ;; Location of send queue.
+   smtpmail-queue-dir
+   (path-join *user-smtpmail-data-directory* "send-queue")
    ;; Use smtpmail as the default method of sending email.
    send-mail-function 'smtpmail-send-it
-   message-send-mail-function 'smtpmail-send-it
-   ;; Queue mail when offline.
-   smtpmail-queue-mail t)
+   message-send-mail-function 'message-smtpmail-send-it)
 
   (use-package smtpmail-multi
     :config
     (validate-setq
      ;; Use smtpmail-multi as the default method of sending email.
      send-mail-function 'smtpmail-send-it
-     message-send-mail-function 'smtpmail-send-it)))
+     message-send-mail-function 'smtpmail-send-it))
+
+  (use-package smtpmail-async
+    :after async
+    :ensure nil
+    :config
+    (validate-setq
+     ;; Default to asynchronous mode.
+     send-mail-function 'async-smtpmail-send-it
+     message-send-mail-function 'async-smtpmail-send-it)))
 
 
 (provide 'utilities/smtpmail)
