dfeca07d2b6305e2173d053fd8c9e8d74cae7dd1
David Holm <david.holm@ericsson.com>
2018-03-07 15:03:17 +0100

    apps: deft: Use helm-deft as a frontend to notes

--- .emacs.d/lisp/apps/deft.el
+++ .emacs.d/lisp/apps/deft.el
@@ -9,11 +9,11 @@
 (use-package deft
   :commands deft
   :bind-wrap
-  (:map deft-mode-map
-        ((:key :basic :open-file-context) . deft-find-file))
+  (((:key :apps :notes) . deft)
+   :map deft-mode-map
+   ((:key :basic :open-file-context) . deft-find-file))
   :init
   (make-directory *user-notes-data-directory* t)
-  (user/bind-key-global :apps :notes 'deft)
   :config
   (validate-setq
    ;; The path to where notes will be stored.
@@ -21,8 +21,16 @@
    ;; Enforce good file naming.
    deft-use-filename-as-title t
    ;; Auto-save idle interval in seconds.
-   deft-auto-save-interval 30.0))
+   deft-auto-save-interval 30.0)
 
+  (use-package helm-deft
+    :quelpa (helm-deft :fetcher github :repo "dfeich/helm-deft")
+    :bind-wrap
+    ((:key :apps :notes) . helm-deft)
+    :config
+    (validate-setq
+     ;; Directories to use.
+     helm-deft-dir-list `(,*user-notes-data-directory*))))
 
 (provide 'apps/deft)
 ;;; deft.el ends here
