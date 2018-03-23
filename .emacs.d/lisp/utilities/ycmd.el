33add5d547ae91d7551e02249462c010f2af2abc
David Holm <david.holm@ericsson.com>
2018-03-23 17:48:31 +0100

    utilities: ycmd: Add ycmd again, but disabled

---
+++ .emacs.d/lisp/utilities/ycmd.el
@@ -0,0 +1,37 @@
+;;; ycmd.el --- Support for ycmd -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package ycmd
+  :disabled
+  :if (executable-find "ycmd")
+  :hook (ycmd-mode-hook . ycmd-eldoc-setup)
+  :init
+  (global-ycmd-mode t)
+  :config
+  (validate-setq
+   ;; Location of `ycmd'.
+   ycmd-server-command `(,(executable-find "ycmd"))
+   ycmd-global-config (path-join *user-home-directory* ".local" "etc"
+                                 "ycmd.py")
+   ;; Always enable completion.
+   ycmd-force-semantic-completion t)
+
+  (use-package company-ycmd
+    :config
+    (company-ycmd-setup))
+
+  (use-package flycheck-ycmd
+    :config
+    (flycheck-ycmd-setup)
+
+    (unless (display-graphic-p)
+      (validate-setq
+       ;; Workaround for interaction bug between company-ycmd and
+       ;; flycheck-ycmd.
+       ;;  - https://github.com/abingham/emacs-ycmd/issues/144
+       flycheck-indication-mode nil))))
+
+
+(provide 'utilities/ycmd)
+;;; ycmd.el ends here
