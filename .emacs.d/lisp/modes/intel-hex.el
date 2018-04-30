8365e3c45d75d287f6f5950c158ab9e6824a7875
David Holm <david.holm@ericsson.com>
2018-04-30 09:05:06 +0200

    modes: Intel hex mode support

---
+++ .emacs.d/lisp/modes/intel-hex.el
@@ -0,0 +1,9 @@
+;;; intel-hex.el --- Initializes Intel hex mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package intel-hex-mode)
+
+
+(provide 'modes/intel-hex)
+;;; intel-hex.el ends here
