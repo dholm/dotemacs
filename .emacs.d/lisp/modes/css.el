31066f829b49d50079ffb2e8e82f69eea20c1aaa
David Holm <david.holm@ericsson.com>
2018-06-11 16:28:28 +0200

    modes/css: CSS LSP support

--- .emacs.d/lisp/modes/css.el
+++ .emacs.d/lisp/modes/css.el
@@ -16,6 +16,13 @@
   (skewer-css-mode t)
   (rainbow-mode t)
 
+  (with-feature 'lsp-css
+    (when (eq major-mode 'css-mode)
+    ;; Only enable in strictly css-mode, not scss-mode (css-mode-hook
+    ;; fires for scss-mode because scss-mode is derived from
+    ;; css-mode).
+    (lsp-css-enable)))
+
   ;;; (Bindings) ;;;
   (user/bind-key-local :nav :functions/toc 'helm-css-scss)
   (user/bind-key-local :nav :go-back 'helm-css-scss-back-to-last-point))
@@ -23,8 +30,7 @@
 
 (use-package css-mode
   :defer
-  :init
-  (add-hook 'css-mode-hook 'user--css-mode-hook)
+  :hook (css-mode-hook . user--css-mode-hook)
   :config
   (use-package showcss-mode
     :quelpa (showcss-mode
@@ -35,6 +41,12 @@
 
   (when (feature-p 'helm)
     (use-package helm-css-scss))
+
+  (use-package lsp-css
+    :if (executable-find "css-languageserver")
+    :quelpa (lsp-css
+             :fetcher github
+             :repo "emacs-lsp/lsp-css"))
 
   (use-package flycheck-css-colorguard
     :config
