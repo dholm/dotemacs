51435cbbc3de7ff3e742d2fe43642bb42de427d7
David Holm <david.holm@ericsson.com>
2018-07-09 17:56:14 +0200

    utilities/lsp: company-lsp dependency bugfix

--- .emacs.d/lisp/utilities/lsp.el
+++ .emacs.d/lisp/utilities/lsp.el
@@ -8,11 +8,16 @@
   (use-package lsp-ui
     :hook (lsp-mode-hook . lsp-ui-mode))
   (use-package company-lsp
-    :after (company-mode)
+    :after (company)
     :config
     (validate-setq
      company-lsp-enable-snippet t
-     company-lsp-cache-candidates t)
+     ;; Enable automatic chained completions.
+     company-lsp-enable-recompletion t
+     ;; Let the server handle caching.
+     company-lsp-async t
+     company-lsp-cache-candidates nil)
+
     (add-to-list 'company-backends 'company-lsp)))
 
 
