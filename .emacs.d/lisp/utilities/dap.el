de14314920c39c7fca9988945712e5b307326e20
David Holm <david.holm@ericsson.com>
2019-02-20 14:14:33 +0100

    utiltities: DAP support

---
+++ .emacs.d/lisp/utilities/dap.el
@@ -0,0 +1,21 @@
+;;; dap.el --- Debug adapter protocol. -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package dap-mode
+  :pin "MELPA"
+  :hook ((lsp-mode-hook . dap-mode)
+         (lsp-mode-hook . dap-ui-mode))
+  :bind-wrap
+  (:map lsp-mode-map
+        ((:key :debug :break) . dap-breakpoint-toggle)
+        ((:key :debug :step) . dap-step-in)
+        ((:key :debug :next) . dap-next)
+        ((:key :debug :run) . dap-debug)
+        ((:key :debug :continue) . dap-continue)
+
+        ((:key :debug :show-value) . dap-ui-inspect-thing-at-point)))
+
+
+(provide 'utilities/dap)
+;;; dap.el ends here
