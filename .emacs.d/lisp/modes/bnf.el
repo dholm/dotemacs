18b80b599059c868f8b1199b036731eb15152054
David Holm <david.holm@ericsson.com>
2019-03-25 09:00:43 +0100

    modes: BNF mode

---
+++ .emacs.d/lisp/modes/bnf.el
@@ -0,0 +1,10 @@
+;;; bnf.el --- Initializes BNF mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package bnf-mode
+  :mode "\\.bnf\\'")
+
+
+(provide 'modes/bnf)
+;;; bnf.el ends here
