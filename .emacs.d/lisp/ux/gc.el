086848503237d3188ba4dd747dc6a121db402c07
David Holm <david.holm@ericsson.com>
2019-04-24 10:07:32 +0200

    ux: Garbage collector configuration

---
+++ .emacs.d/lisp/ux/gc.el
@@ -0,0 +1,11 @@
+;;; gc.el --- Garbage collector settings -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package gcmh
+  :config
+  (gcmh-mode t))
+
+
+(provide 'ux/gc)
+;;; gc.el ends here
