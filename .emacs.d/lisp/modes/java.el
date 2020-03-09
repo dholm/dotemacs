be04f27e2d183ef38f16d3b1138536a9146b2254
David Holm <david.holm@ericsson.com>
2020-03-09 17:27:30 +0100

    modes: Java support

---
+++ .emacs.d/lisp/modes/java.el
@@ -0,0 +1,24 @@
+;;; java.el --- Java mode support -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(defun user--java-mode-hook ()
+  "Java mode hook."
+  (with-feature 'lsp
+    (lsp)))
+
+(use-package cc-mode
+  :ensure nil
+  :if (executable-find "javac")
+  :defer
+  :hook (java-mode-hook . user--java-mode-hook)
+  :config
+  (use-package lsp-java
+    :config
+    (validate-setq
+     ;; Path to LSP server.
+     lsp-java-server-install-dir (path-join *user-cache-directory* "lsp-java"))))
+
+
+(provide 'modes/java)
+;;; java.el ends here
