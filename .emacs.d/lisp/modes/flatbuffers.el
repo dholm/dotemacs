8523353dfb900d2f592073a61c9f6c3b63d3fa26
David Holm <david.holm@ericsson.com>
2020-08-31 14:37:00 +0200

    lisp/modes: Flatbuffers support

---
+++ .emacs.d/lisp/modes/flatbuffers.el
@@ -0,0 +1,11 @@
+;;; flatbuffers.el --- Flatbuffers mode support -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package flatbuffers-mode
+  :defer
+  :mode (("fbs\\'" . flatbuffers-mode)))
+
+
+(provide 'modes/flatbuffers)
+;;; flatbuffers.el ends here
