71ed62428931928f542cb80d693df3784825f370
David Holm <david.holm@ericsson.com>
2020-08-31 14:39:22 +0200

    modes/kconfig: KConfig mode support

---
+++ .emacs.d/lisp/modes/kconfig.el
@@ -0,0 +1,9 @@
+;;; kconfig --- Initializes Kconfig mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package kconfig-mode)
+
+
+(provide 'modes/kconfig)
+;;; kconfig.el ends here
