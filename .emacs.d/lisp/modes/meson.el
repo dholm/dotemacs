241a6797b414980f3dfc984d950797917b71605d
David Holm <david.holm@ericsson.com>
2020-09-04 09:31:15 +0200

    modes: Meson mode

---
+++ .emacs.d/lisp/modes/meson.el
@@ -0,0 +1,10 @@
+;;; meson --- Initializes Meson mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package meson-mode
+  :hook (meson-mode-hook . company-mode))
+
+
+(provide 'modes/meson)
+;;; meson.el ends here
