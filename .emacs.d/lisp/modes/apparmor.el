729982e7fabc5042de034f61da81890a3f6e95da
David Holm <david.holm@ericsson.com>
2020-12-21 10:01:26 +0100

    modes: AppArmor profile mode

---
+++ .emacs.d/lisp/modes/apparmor.el
@@ -0,0 +1,11 @@
+;;; apparmor.el --- Apparmor mode -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package apparmor-mode
+  :defer t
+  :mode ("apparmor" . apparmor-mode))
+
+
+(provide 'modes/apparmor)
+;;; apparmor.el ends here
