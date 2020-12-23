b76e658c7dfaa40703722996e3d1d58a73f90bd9
David Holm <david.holm@ericsson.com>
2020-12-23 15:53:37 +0100

    modes/tex: Use :if from use-package

--- .emacs.d/lisp/modes/tex.el
+++ .emacs.d/lisp/modes/tex.el
@@ -49,8 +49,6 @@
   ;;; (Bindings) ;;;
   (when (feature-p 'latexdiff)
     (user/bind-key-local :util :diff 'helm-latexdiff))
-  (when (feature-p 'ltx-help)
-    (user/bind-key-local :doc :reference 'latex-help))
   (user/bind-key-local :nav :functions/toc 'reftex-toc)
   (when (display-graphic-p)
     (user/bind-key-local :code :run 'preview-document))
@@ -77,186 +75,181 @@
   "BibTeX mode hook.")
 
 
-(when (or (executable-find "tex")
+(use-package tex-mode
+  :if (or (executable-find "tex")
           (executable-find "latex"))
-  (use-package tex-mode
-    :defer
-    :hook
-    ((tex-mode-hook . user--tex-mode-hook)
-     (TeX-mode-hook . user--tex-mode-hook)
-     (latex-mode-hook . user--latex-mode-hook)
-     (LaTeX-mode-hook . user--latex-mode-hook)
-     (bibtex-mode-hook . user--bibtex-mode-hook))
+  :defer
+  :hook
+  ((tex-mode-hook . user--tex-mode-hook)
+   (TeX-mode-hook . user--tex-mode-hook)
+   (latex-mode-hook . user--latex-mode-hook)
+   (LaTeX-mode-hook . user--latex-mode-hook)
+   (bibtex-mode-hook . user--bibtex-mode-hook))
+  :config
+  (with-eval-after-load 'mode-compile
+    (setq mode-compile-modes-alist
+          (append '((latex-mode . (tex-compile kill-compilation)))
+                  mode-compile-modes-alist)))
+
+  ;;; (Packages) ;;;
+  (use-package reftex
+    :ensure nil
+    :diminish reftex-mode
     :config
-    (with-eval-after-load 'mode-compile
-      (setq mode-compile-modes-alist
-            (append '((latex-mode . (tex-compile kill-compilation)))
-                    mode-compile-modes-alist)))
+    (use-package company-reftex
+      :after (company)))
 
-    ;;; (Packages) ;;;
-    (use-package reftex
-      :ensure nil
-      :diminish reftex-mode
-      :config
-      (use-package company-reftex
-        :after (company)))
+  (use-package latex-extra
+    :hook (LaTeX-mode-hook . latex-extra-mode))
 
-    (use-package latex-extra
-      :hook (LaTeX-mode-hook . latex-extra-mode))
+  (use-package latexdiff
+    :if (executable-find "latexdiff"))
 
-    (use-package latexdiff
-      :if (executable-find "latexdiff"))
+  (use-package bibtex
+    :config
+    (validate-setq
+     ;; (BibTeX) ;;
+     bibtex-autokey-name-case-convert 'identity
+     bibtex-autokey-year-length 4)
 
-    (use-package bibtex
-      :config
+    (use-package gscholar-bibtex)
+    (use-package bibtex-utils)
+    (use-package bibclean-format
+      :if (executable-find "bibclean")
+      :hook (bibtex-mode-hook . bibclean-format-on-save-mode)))
+
+  (use-package auctex
+    :bind (:map LaTeX-mode-map
+                ([remap fill-paragraph] . LaTeX-fill-paragraph))
+    :config
+    (validate-setq
+     ;; Use synctex to communicate with LaTeX.
+     TeX-source-correlate-method 'synctex
+     LaTeX-command "latex -synctex=1 -shell-escape"
+     ;; Do not ask about saving buffers before starting TeX.
+     TeX-save-query nil
+     ;; Use PDF rather than DVI by default.
+     TeX-PDF-mode t
+     TeX-interactive-mode t
+     ;; Parse file after load/save unless it has a style hook.
+     TeX-parse-self t
+     TeX-auto-save t
+     ;; Display help on error messages.
+     TeX-display-help t
+     ;; Automatically remove tabs.
+     TeX-auto-untabify t
+     ;; Support for backwards search in documents.
+     TeX-source-correlate-mode t
+     TeX-source-correlate-start-server t
+     TeX-master nil
+
+     ;; (RefTeX) ;;
+     ;; Make reftex interact with AucTeX.
+     reftex-plug-into-AUCTeX t
+     ;; Prompt for optional arguments.
+     reftex-cite-prompt-optional-args t
+     ;; Try to guess the label type before prompting.
+     reftex-guess-label-type t
+     ;; Use nice fonts for toc.
+     reftex-use-fonts t
+     ;; Revisit files if necessary when browsing toc.
+     reftex-revisit-to-follow t
+     ;; Center on the section currently being edited.
+     reftex-auto-recenter-toc t
+     ;; Save parse info to avoid reparsing every time a file is visited.
+     reftex-save-parse-info t
+     ;; Cache selection buffers for faster access.
+     reftex-use-multiple-selection-buffers t)
+
+    (with-eval-after-load 'preview
+      ;; Support previewing of TikZ.
+      (add-to-list 'preview-default-preamble "\\PreviewEnvironment{tikzpicture}" t))
+
+    (with-eval-after-load 'auctex
+      (when (user/company-mode-p)
+        (with-feature 'company-auctex
+          ;; Enable company AUCTeX completion.
+          (company-auctex-init))))
+
+    (cond
+     ((eq system-type 'darwin)
       (validate-setq
-       ;; (BibTeX) ;;
-       bibtex-autokey-name-case-convert 'identity
-       bibtex-autokey-year-length 4)
+       TeX-view-program-list (quote (("Preview" "open -a Preview.app %o")
+                                     ("Skim" "open -a Skim.app %o")
+                                     ("open" "open %o")))
+       TeX-view-program-selection '((output-pdf "Preview"))))
+     (t (with-executable 'evince
+          (validate-setq
+           TeX-view-program-list '(("Evince" "evince --page-index=%(outpage) %o"))
+           TeX-view-program-selection '((output-pdf "Evince"))))))
 
-      (use-package gscholar-bibtex)
-      (use-package bibtex-utils)
-      (use-package bibclean-format
-        :if (executable-find "bibclean")
-        :hook (bibtex-mode-hook . bibclean-format-on-save-mode)))
-
-    (use-package auctex
-      :bind (:map LaTeX-mode-map
-                  ([remap fill-paragraph] . LaTeX-fill-paragraph))
-      :config
-      (validate-setq
-       ;; Use synctex to communicate with LaTeX.
-       TeX-source-correlate-method 'synctex
-       LaTeX-command "latex -synctex=1 -shell-escape"
-       ;; Do not ask about saving buffers before starting TeX.
-       TeX-save-query nil
-       ;; Use PDF rather than DVI by default.
-       TeX-PDF-mode t
-       TeX-interactive-mode t
-       ;; Parse file after load/save unless it has a style hook.
-       TeX-parse-self t
-       TeX-auto-save t
-       ;; Display help on error messages.
-       TeX-display-help t
-       ;; Automatically remove tabs.
-       TeX-auto-untabify t
-       ;; Support for backwards search in documents.
-       TeX-source-correlate-mode t
-       TeX-source-correlate-start-server t
-       TeX-master nil
-
-       ;; (RefTeX) ;;
-       ;; Make reftex interact with AucTeX.
-       reftex-plug-into-AUCTeX t
-       ;; Prompt for optional arguments.
-       reftex-cite-prompt-optional-args t
-       ;; Try to guess the label type before prompting.
-       reftex-guess-label-type t
-       ;; Use nice fonts for toc.
-       reftex-use-fonts t
-       ;; Revisit files if necessary when browsing toc.
-       reftex-revisit-to-follow t
-       ;; Center on the section currently being edited.
-       reftex-auto-recenter-toc t
-       ;; Save parse info to avoid reparsing every time a file is visited.
-       reftex-save-parse-info t
-       ;; Cache selection buffers for faster access.
-       reftex-use-multiple-selection-buffers t)
-
-      (with-eval-after-load 'preview
-        ;; Support previewing of TikZ.
-        (add-to-list 'preview-default-preamble "\\PreviewEnvironment{tikzpicture}" t))
-
-      (with-eval-after-load 'auctex
-        (when (user/company-mode-p)
-          (with-feature 'company-auctex
-            ;; Enable company AUCTeX completion.
-            (company-auctex-init))))
-
-      (cond
-       ((eq system-type 'darwin)
-        (validate-setq
-         TeX-view-program-list (quote (("Preview" "open -a Preview.app %o")
-                                       ("Skim" "open -a Skim.app %o")
-                                       ("open" "open %o")))
-         TeX-view-program-selection '((output-pdf "Preview"))))
-       (t (with-executable 'evince
-            (validate-setq
-             TeX-view-program-list '(("Evince" "evince --page-index=%(outpage) %o"))
-             TeX-view-program-selection '((output-pdf "Evince"))))))
-
-      ;;; (Functions) ;;;
-      (defadvice LaTeX-fill-region-as-paragraph (around LaTeX-sentence-filling)
-        "Start each sentence on a new line.
+    ;;; (Functions) ;;;
+    (defadvice LaTeX-fill-region-as-paragraph (around LaTeX-sentence-filling)
+      "Start each sentence on a new line.
 
 Makes it easier to version control LaTeX-files."
-        (let ((from (ad-get-arg 0))
-              (to-marker (set-marker (make-marker) (ad-get-arg 1)))
-              tmp-end)
-          (while (< from (marker-position to-marker))
-            (forward-sentence)
-            ;; might have gone beyond to-marker --- use whichever is smaller:
-            (ad-set-arg 1 (setq tmp-end (min (point) (marker-position to-marker))))
-            ad-do-it
-            (ad-set-arg 0 (setq from (point)))
-            (unless (or
-                     (bolp)
-                     (looking-at "\\s *$"))
-              (LaTeX-newline)))
-          (set-marker to-marker nil)))
+      (let ((from (ad-get-arg 0))
+            (to-marker (set-marker (make-marker) (ad-get-arg 1)))
+            tmp-end)
+        (while (< from (marker-position to-marker))
+          (forward-sentence)
+          ;; might have gone beyond to-marker --- use whichever is smaller:
+          (ad-set-arg 1 (setq tmp-end (min (point) (marker-position to-marker))))
+          ad-do-it
+          (ad-set-arg 0 (setq from (point)))
+          (unless (or
+                   (bolp)
+                   (looking-at "\\s *$"))
+            (LaTeX-newline)))
+        (set-marker to-marker nil)))
 
       ;;; (Packages) ;;;
-      (use-package auctex-latexmk
-        :if (executable-find "latexmk")
-        :init
-        (auctex-latexmk-setup))
+    (use-package auctex-latexmk
+      :if (executable-find "latexmk")
+      :init
+      (auctex-latexmk-setup))
 
-      (use-package auctex-lua
-        :if (executable-find "luatex"))
+    (use-package auctex-lua
+      :if (executable-find "luatex"))
 
-      (use-package company-auctex
-        :after (company)
-        :init
-        (company-auctex-init))
+    (use-package company-auctex
+      :after (company)
+      :init
+      (company-auctex-init))
 
-      (use-package bibretrieve))
+    (use-package bibretrieve))
 
-    (use-package ebib
-      :config
-      ;; Cannot be validated as it breaks the specification of
-      ;; `ebib-file-search-dirs'.
-      (setq
-       ebib-file-search-dirs
-       '(lambda ()
-          (with-project project (path-buffer-abs)
-            (user/proj-root project)))))
+  (use-package ebib
+    :config
+    ;; Cannot be validated as it breaks the specification of
+    ;; `ebib-file-search-dirs'.
+    (setq
+     ebib-file-search-dirs
+     '(lambda ()
+        (with-project project (path-buffer-abs)
+          (user/proj-root project)))))
 
-    (use-package zotelo
-      :diminish zotelo-minor-mode
-      :hook (TeX-mode-hook . zotelo-minor-mode))
+  (use-package zotelo
+    :diminish zotelo-minor-mode
+    :hook (TeX-mode-hook . zotelo-minor-mode))
 
-    (use-package ac-math
-      :after (auto-complete)
-      :config
-      (validate-setq
-       ;; Enable unicode math input.
-       ac-math-unicode-in-math-p t))
+  (use-package ac-math
+    :after (auto-complete)
+    :config
+    (validate-setq
+     ;; Enable unicode math input.
+     ac-math-unicode-in-math-p t))
 
-    (use-package auto-complete-latex
-      :after (auto-complete)
-      :quelpa (auto-complete-latex
-               :fetcher hg
-               :url "https://bitbucket.org/tequilasunset/auto-complete-latex")
-      :config
-      (add-ac-modes 'latex-mode 'LaTeX-mode))
+  (use-package auto-complete-latex
+    :after (auto-complete)
+    :quelpa (auto-complete-latex
+             :fetcher hg
+             :url "https://bitbucket.org/tequilasunset/auto-complete-latex")
+    :config
+    (add-ac-modes 'latex-mode 'LaTeX-mode))
 
-    (use-package company-math
-      :after (company))
-
-    (use-package ltx-help
-      :quelpa (ltx-help
-               :fetcher url
-               :url "http://mirror.switch.ch/ftp/mirror/tex/info/latex2e-help-texinfo/ltx-help.el")))
+  (use-package company-math
+    :after (company))
 
   (use-package texfrag
     :diminish texfrag-mode
