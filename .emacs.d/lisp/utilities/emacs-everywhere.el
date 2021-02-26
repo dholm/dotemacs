a1b6a65484d70ae33af20b58824323e34df1b6cd
David Holm <david.holm@ericsson.com>
2021-02-26 14:06:07 +0100

    lisp/utilities: Install emacs-everywhere

---
+++ .emacs.d/lisp/utilities/emacs-everywhere.el
@@ -0,0 +1,10 @@
+;;; emacs-everywhere.el --- Integrate Emacs with any application -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package emacs-everywhere
+  :ensure t)
+
+
+(provide 'utilities/emacs-everywhere)
+;;; emacs-everywhere.el ends here
