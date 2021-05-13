8bcb035123e06017f0658a24153c65968eda5507
David Holm <david.holm@ericsson.com>
2021-05-13 09:41:12 +0200

    utilities: Add transient configuration

---
+++ .emacs.d/lisp/utilities/transient.el
@@ -0,0 +1,16 @@
+;;; transient.el --- Configuration for transient. -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package transient
+  :ensure nil
+  :config
+  (validate-setq
+   ;; Location of transient files.
+   transient-history-file (path-join *user-cache-directory* "transient" "history.el")
+   transient-values-file (path-join *user-cache-directory* "transient" "values.el")
+   transient-levels-file (path-join *user-cache-directory* "transient" "levels.el")))
+
+
+(provide 'utilities/transient)
+;;; transient.el ends here
