783b95b28bdcbf6c4bdedeb80afbf2fc9c5cf065
David Holm <david.holm@ericsson.com>
2021-05-13 09:48:14 +0200

    utilities/transient: Create cache folder

--- .emacs.d/lisp/utilities/transient.el
+++ .emacs.d/lisp/utilities/transient.el
@@ -9,7 +9,10 @@
    ;; Location of transient files.
    transient-history-file (path-join *user-cache-directory* "transient" "history.el")
    transient-values-file (path-join *user-cache-directory* "transient" "values.el")
-   transient-levels-file (path-join *user-cache-directory* "transient" "levels.el")))
+   transient-levels-file (path-join *user-cache-directory* "transient" "levels.el"))
+
+  ;; Create the transient cache folder.
+  (make-directory (path-join *user-cache-directory* "transient") t))
 
 
 (provide 'utilities/transient)
