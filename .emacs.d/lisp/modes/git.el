7b70152e89129dbf704a22f488bf969c0c442c87
David Holm <david.holm@ericsson.com>
2021-11-05 11:18:04 +0100

    modes: Modes for Git configuration files

---
+++ .emacs.d/lisp/modes/git.el
@@ -0,0 +1,10 @@
+;;; git.el --- Initializes modes for Git configuration files -*- lexical-binding: t; -*-
+;;; Commentary:
+;;; Code:
+
+(use-package git-modes
+  :defer)
+
+
+(provide 'modes/git)
+;;; git.el ends here
