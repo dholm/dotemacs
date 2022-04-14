d59647b689c7f3543b04131156e59a53270dab25
David Holm <david.holm@ericsson.com>
2022-04-14 19:16:09 +0200

    modes/rust: Switch from rust-mode to rustic

--- .emacs.d/lisp/modes/rust.el
+++ .emacs.d/lisp/modes/rust.el
@@ -2,27 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
-(defun user--rust-mode-hook ()
-  "Rust mode hook."
-  (with-feature 'rusti
-    (rusti-minor-mode t)))
-
-(use-package rust-mode
+(use-package rustic
   :if (executable-find "rustc")
   :defer
-  :init
-  (add-hook 'rust-mode-hook 'user--rust-mode-hook)
-  :config
-    ;;; (Packages) ;;;
-  (use-package flycheck-rust)
-  (use-package rusti
-    :quelpa (rusti
-             :fetcher github
-             :repo "ruediger/rusti.el"))
-  (use-package lsp-rust
-    :ensure nil
-    :if (executable-find "rls")
-    :hook (rust-mode-hook . lsp-mode)))
+  :mode ("\\.rs$" . rustic-mode))
 
 
 (provide 'modes/rust)
